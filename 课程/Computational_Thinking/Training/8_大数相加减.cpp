0b4f2cb3957c8bd77c8c608636fd031c2a0e5b60
Hazuki-295 <luosheng233@outlook.com>
2021-05-19 22:23:27 +0800

    Update 8_大数相加减.cpp

--- 课程/Computational_Thinking/Training/8_大数相加减.cpp
+++ 课程/Computational_Thinking/Training/8_大数相加减.cpp
@@ -5,41 +5,44 @@
 #include <algorithm>
 using namespace std;
 
-void bigAdd(const string num1, const string num2, vector<int> &result)
+void bigAdd(const string num1, const string num2, vector<int> &result) // 两大正数相加
 {
 	/* 从低位到高位依次计算 */
-	auto p1 = num1.rbegin(), p2 = num2.rbegin();
-	for (; p1 != num1.rend() && p2 != num2.rend(); p1++, p2++)
-		result.push_back((*p1 - '0') + (*p2 - '0'));
-	if (p2 == num2.rend())                // num2较短
-		for (; p1 != num1.rend(); p1++)
-			result.push_back(*p1 - '0');
-	else                                  // num1较短
-		for (; p2 != num2.rend(); p2++)
-			result.push_back(*p2 - '0');
-
-	/* 进位处理 */
-	for (int i = 0; i <= result.size() - 2; i++)
+	auto p1 = num1.begin(), p2 = num2.begin();
+	for (vector<int>::size_type i = 0; i < result.size() - 1; i++) // 保证i+1在vector合法范围
 	{
-		int temp = result[i] / 10;  // 存放进位的数
-		result[i] = result[i] % 10; // 余数
-		result[i + 1] = result[i + 1] + temp; // i+1最大等于size-1，i最大等于size-2
-	}
-
-	/* 处理最大位 */
-	int *last = &result.back();
-	while (*last >= 10) // 若加法的结果大于等于10，则把进位的1加入到下一位的计算中
-	{
-		int temp = *last / 10;  // 存放进位的数
-		*last = *last % 10;     // 余数
-		result.push_back(temp);
-		last++;
+		if (p1 != num1.end() && p2 != num2.end()) // 未到达结尾
+		{
+			int temp = (*p1 - '0') + (*p2 - '0');
+			result[i] += temp;
+			if (result[i] >= 10)
+			{
+				result[i + 1] = 1; // 进位
+				result[i] %= 10;   // 余数
+			}
+			p1++; p2++;
+		}
 	}
 }
 
-void bigSubtract(const string num1, const string num2, vector<int> &result)
+void bigSubtract(const string num1, const string num2, vector<int> &result) // 两大正数相减(num1>num2)
 {
-
+	/* 从低位到高位依次计算 */
+	auto p1 = num1.begin(), p2 = num2.begin();
+	for (vector<int>::size_type i = 0; i < result.size() - 1; i++) // 保证i+1在vector合法范围
+	{
+		if (p1 != num1.end() && p2 != num2.end()) // 未到达结尾
+		{
+			int temp = (*p1 - '0') - (*p2 - '0');
+			result[i] += temp;
+			if (result[i] < 0)
+			{
+				result[i + 1] = -1;  // 借位
+				result[i] += 10;     // 余数
+			}
+			p1++; p2++;
+		}
+	}
 }
 
 int main()
@@ -49,7 +52,7 @@ int main()
 	while (getline(cin, line)) // 处理一行
 	{
 		string num1, num2;
-		vector<int> result;
+
 		istringstream buffer(line);  // 将buffer绑定到刚读入的行
 
 		while (buffer >> num1 >> num2) // 模拟加法
@@ -59,6 +62,24 @@ int main()
 			if (num1[0] == '-') { negative1 = true; num1.erase(num1.begin()); }
 			if (num2[0] == '-') { negative2 = true; num2.erase(num2.begin()); }
 
+			auto maxsize = max(num1.size(), num2.size());
+			vector<int> result(maxsize + 1); // 创建结果数组
+
+			/* 预处理 */
+			string copy1(num1); string copy2(num2); // 拷贝，作正负判断时使用
+
+			reverse(num1.begin(), num1.end()); // 倒序存储，各位对齐
+			reverse(num2.begin(), num2.end());
+
+			while (num1.size() != maxsize || num2.size() != maxsize) // 扩充以方便处理
+			{
+				if (num1.size() != maxsize) // 缺项补0
+					num1.push_back('0');
+				else if (num2.size() != maxsize)
+					num2.push_back('0');
+			}
+			/* 预处理 */
+
 			if (negative1 == false && negative2 == false) // 均为正数
 				bigAdd(num1, num2, result);
 			else if (negative1 == true && negative2 == true) // 均为负数
@@ -66,14 +87,58 @@ int main()
 				bigAdd(num1, num2, result);
 				cout << "-"; // 输出负号
 			}
+			else // 一正一负，
+			{
+				bool copy1_copy2 = true;              // 假设copy1>copy2成立
+
+				if (copy1.size() < copy2.size())
+				{
+					copy1_copy2 = false;
+				}
+				else if (copy1.size() == copy2.size()) // 等长
+				{
+					if (copy1 == copy2) copy1_copy2 = false;
+					auto p1 = copy1.begin(), p2 = copy2.begin();
+					while (p1 != copy1.end() && p2 != copy2.end())      // 未到达结尾
+						if (*p1 < *p2) { copy1_copy2 = false; break; }
+						else if (*p1 == *p2) { p1++; p2++; }
+						else if (*p1 > *p2) { break; }
+				}
+
+				if (negative1 == false && negative2 == true) // 正加负。
+				{
+					if (copy1_copy2 || copy1 == copy2)
+						bigSubtract(num1, num2, result);
+					else
+					{
+						bigSubtract(num2, num1, result);
+						cout << "-"; // 输出负号
+					}
+				}
+				else if (negative1 == true && negative2 == false) // 负加正。
+				{
+					if (copy1_copy2)
+					{
+						bigSubtract(num1, num2, result);
+						cout << "-"; // 输出负号
+					}
+					else
+						bigSubtract(num2, num1, result);
+				}
+			}
+
+			auto highest = result.end() - 1;
+			while (result.size() > 1 && *highest == 0) // 去掉前缀的0
+			{
+				result.erase(highest);
+				highest = result.end() - 1;
+			}
 
 			for (auto num = result.rbegin(); num != result.rend(); num++)
 				cout << *num;
 			cout << endl;
 		}
 	}
-
-
 
 	return 0;
 }
