755e585d73433543c86f9482fa589b09fa92396e
Hazuki-295 <luosheng233@outlook.com>
2021-05-20 19:35:19 +0800

    Update 7_语料字典.cpp

--- 课程/Computational_Thinking/Training/7_语料字典.cpp
+++ 课程/Computational_Thinking/Training/7_语料字典.cpp
@@ -7,45 +7,49 @@ using namespace std;
 
 typedef struct { string word; unsigned count; } record, *precord;
 
-//bool cmp(record a, record b) { return (a.word < b.word); } // 升序
-//bool cmp2(record a, record b) { return (a.count > b.count); } // 降序
-int cmp(const void *a, const void *b) // 升序
+bool operator> (const record &a, const record &b) // 按单词出现次数及字典顺序排序
 {
-	if (((precord)a)->word > ((precord)b)->word)
-		return 1;
-	else if (((precord)a)->word == ((precord)b)->word)
-		return 0;
-	else
-		return -1;
+	if (a.count > b.count) return true; // 按单词出现次数
+	else if (a.count == b.count)
+		if (a.word < b.word) // 按字典序
+			return true;
+
+	return false;
 }
 
-int cmp2(const void *a, const void *b) // 降序
-{
-	if (((precord)a)->count < ((precord)b)->count)
-		return 1;
-	else if (((precord)a)->count == ((precord)b)->count)
-		return 0;
-	else
-		return -1;
-}
+bool cmp(record a, record b) { return a > b; } // 降序
 
 int main()
 {
-	string line; stringstream buffer;
+	string line;
 	vector<record> dictionary;
 
 	while (getline(cin, line)) // 处理一行
 	{
-		string word;                 // 单个单词
-		istringstream buffer(line);  // 将buffer绑定到刚读入的行
+		string word; // 单个单词
+
+		/* 预处理 */
+		for (auto &c : line)
+		{
+			if (c == '\'' || c == '-')
+				continue;
+			else if (isdigit(c) || iscntrl(c)) // 去掉数字、控制符
+				c = ' ';
+			else if (!isgraph(c) || isspace(c))
+				c = ' ';
+			else if (isalpha(c))
+				c = tolower(c);    // 小写形式
+		}
+
+		stringstream buffer(line);  // 将buffer绑定到刚读入的行
 
 		while (buffer >> word)
 		{
-			// 处理单词
-			for (auto c = word.begin(); c != word.end(); )
+			/* 提取单词 */
+			auto c = word.begin();
+			while (c != word.end())
 			{
-				*c = tolower(*c);    // 小写形式
-				if (*c == '\'')      // 去掉连字符后面的单词
+				if (*c == '\'') // 去掉所有格
 				{
 					word.erase(c, word.end());
 					break;
@@ -53,28 +57,24 @@ int main()
 				if (ispunct(*c))         // 去掉标点符号
 				{
 					word.erase(c);
-					if (c == word.end()) // 删掉结尾赶紧退出
+					if (c == word.end()) // 删掉结尾退出
 						break;
 					else
 						continue;
 				}
 				c++;
 			}
-			// auto it = find(dictionary.begin(), dictionary.end(), word);
+
 			vector<record>::iterator it;
 			for (it = dictionary.begin(); it != dictionary.end() && it->word != word; it++);
-
 			if (it == dictionary.end()) // 第一次出现
 				dictionary.push_back({ word , 1 });
 			else
 				it->count++;
 		}
 	}
-	//sort(dictionary.begin(), dictionary.end(), cmp);  // 按字母排序
-	//sort(dictionary.begin(), dictionary.end(), cmp2); // 按单词出现次数排序
 
-	qsort(&dictionary[0], dictionary.size(), sizeof(record), cmp);  // 按字母排序
-	qsort(&dictionary[0], dictionary.size(), sizeof(record), cmp2); // 按单词出现次数排序
+	sort(dictionary.begin(), dictionary.end(), cmp);  // 按单词出现次数及字典顺序排序
 
 	for (auto temp : dictionary)
 		cout << temp.word << endl;
