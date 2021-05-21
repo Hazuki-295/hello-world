dde1b81f8787c53585116a182fc020e421b76bcd
Hazuki-295 <luosheng233@outlook.com>
2021-05-21 18:18:39 +0800

    Update 1_最小差元素.cpp

--- 课程/Computational_Thinking/Training/1_最小差元素.cpp
+++ 课程/Computational_Thinking/Training/1_最小差元素.cpp
@@ -1,37 +1,29 @@
 #include <iostream>
 using namespace std;
 
-typedef struct node {
-	long long value;
-	int pos;
-} Data;
+typedef struct { long long value; int pos; } Number, *pNumber;
 
-int cmp(const void *a, const void *b)
+int compare(const void *a, const void *b)
 {
-	if (((Data *)a)->value < ((Data *)b)->value)
-	{
+	if (((pNumber)a)->value < ((pNumber)b)->value)
 		return -1;
-	}
-	else if (((Data *)a)->value == ((Data *)b)->value)
-	{
+	else if (((pNumber)a)->value == ((pNumber)b)->value)
 		return 0;
-	}
 	else
 		return 1;
 }
 
 int main()
 {
-	int n;
-	cin >> n;
-	Data *data = (Data *)malloc(sizeof(Data) * n);
+	int n; cin >> n;
+	data = (pNumber)malloc(sizeof(Number) * n);
 	
 	for (int i = 0; i < n; i++)
 	{
 		cin >> data[i].value;
 		data[i].pos = i + 1; // 索引从1开始计数
 	}
-	qsort(data, n, sizeof(Data), cmp); // 按值快速排序
+	qsort(data, n, sizeof(Number), compare); // 按值快速排序
 
 	long long min = llabs(data[1].value - data[0].value); // 初始化最小差
 	int index1 = data[1].pos; // 最小差下标
