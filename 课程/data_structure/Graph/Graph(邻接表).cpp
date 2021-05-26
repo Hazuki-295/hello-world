55260fd4d160e87e9ac0fea394e31792cd1ad69c
Hazuki-295 <luosheng233@outlook.com>
2021-05-26 16:02:13 +0800

    Update Graph(邻接表).cpp

--- 课程/data_structure/Graph/Graph(邻接表).cpp
+++ 课程/data_structure/Graph/Graph(邻接表).cpp
@@ -1,266 +1,135 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <queue>
+#include "../../../叶月_hazuki/GitHub/hello-world/课程/data_structure/List/LinkList.cpp"
 using namespace std;
 
 #define MaxVertexNum 100  // 图中顶点数目的最大值
 
-/* 列表节点模板类（以单链表形式实现） */
-template <typename T> struct ListNode {
+/* 边表结点模板类 */
+template <typename InfoType> struct ArcNode {
 	// 成员
-	T data; ListNode *next; // 数据域、指针域
+	int adjvex; InfoType info; // 该弧所指向的顶点的位置(下标)、边权值，next域由链表所有
 	// 构造函数
-	ListNode() : data(0), next(nullptr) {}
-	ListNode(T x) : data(x), next(nullptr) {}
-	ListNode(T x, ListNode *next) : data(x), next(next) {}
+	ArcNode(int _adjvex, InfoType _info) :adjvex(_adjvex), info(_info) {};
 };
-template <typename T> using LinkList = ListNode<T> *; // 单链表(头指针)
 
-/* 链表模板类 */
-template <typename T> class List {
-
-private:
-	using ElemType = T;
-	using LinkList = LinkList<T>;
-	using ListNode = ListNode<T>;
-
-	LinkList L; // L为单链表的头指针(结构指针类型，指向链表的第一个结点)
-
-public:
-	// 构造函数
-	List() { InitList(); }; // 默认构造一个空链表
-
-	/* 初始化一个空链表 */
-	void InitList() // 默认构造一个空链表
-	{
-		L = (LinkList)malloc(sizeof(ListNode));  // 创建头结点
-		L->next = nullptr;                    // 初始为空链表
-	}
-
-	/* 头插法，逆向建立单链表 */
-	LinkList List_HeadInsert(int n)
-	{
-		ListNode *s; ElemType temp;
-		L = (LinkList)malloc(sizeof(ListNode));  // 创建头结点
-		L->next = nullptr;                    // 初始为空链表
-		for (int i = 0; i < n; i++)
-		{
-			cin >> temp;
-
-			s = (ListNode *)malloc(sizeof(ListNode)); // 创建新结点
-			s->data = temp;
-			s->next = L->next; // 将新结点插入表中
-			L->next = s;       // 每次将s所指结点插在最前端
-		}
-		return L; // 返回头指针
-		return L; // 返回头指针
-	}
-
-	/* 尾插法，正向建立单链表 */
-	LinkList List_TailInsert(int n)
-	{
-		ElemType temp;
-		L = (LinkList)malloc(sizeof(ListNode)); // 创建头结点
-		ListNode *s, *r = L;  // r为表尾指针(初始化指向头结点)
-		for (int i = 0; i < n; i++)
-		{
-			cin >> temp;
-
-			s = (ListNode *)malloc(sizeof(ListNode)); // 创建新结点
-			s->data = temp; // s结构指针有数据域，但其指针域未初始化(若有下一个结点，则会被下一轮循环赋予地址，也就是当前位置的下一条语句)
-			r->next = s;    // 当前表尾结点的指针域 r->next 内，存放指向新结点的指针
-			r = s;          // r指向新的表尾结点(保持r仍为表尾指针)
-		}
-		r->next = nullptr; // 尾结点指针置空
-		return L;          // 返回头指针
-	}
-
-	/* 求单链表表长 */
-	int LinkLength()
-	{
-		ListNode *p = L->next; int j = 0; // j为计数器
-		while (p != nullptr) // 从第一个结点开始，计数据结点的个数
-		{
-			j++;
-			p = p->next;
-		}
-		return j;
-	}
-
-	/* 按序号查找 */
-	ListNode *GetElem(int x) // L为"带头结点"的单链表的头指针
-	{
-		ListNode *p; int j;
-
-		p = L->next; j = 1; // 初始化，p指向第一个结点，j为计数器
-
-		if (x == 0)
-			return L;       // 若x等于0，则返回头结点
-		if (x < 1)
-			return nullptr; // 若x无效，则直接返回NULL
-		while (p != nullptr && j < x)  // 从第1个结点开始找，直到 p指向第x个元素(j=x) 或 p为空(达到尾结点)
-		{
-			p = p->next;
-			j++;
-		}
-		return p; // 若找到，则返回第x个结点的指针；若x大于表长，返回NULL
-	}
-
-	/* 按值查找 */
-	ListNode *LocateElem(ElemType e)
-	{
-		ListNode *p = L->next;
-		while (p != nullptr && (p->data != e)) // 从第1个结点开始查找 data域为e 的结点
-		{
-			p = p->next;
-		}
-		return p; // 找到后返回该结点的指针，否则返回NULL
-	}
-
-	/* 插入操作，在表L中第x个位置之前插入新的数据元素e */
-	bool ListInsert(int x, ElemType e)
-	{
-		ListNode *p, *s;
-		p = GetElem(x - 1); // 查找插入位置的前驱结点
-		if (!p) // 插入位置不合法
-			return false;
-
-		s = (ListNode *)malloc(sizeof(ListNode)); // 创建新结点
-		s->data = e;
-		s->next = p->next; // 处理新结点
-		p->next = s;       // 原直接前驱指向新结点
-		return true;
-	}
-
-	/* 删除操作，删除表L中第x个位置的元素，并用e返回删除元素的值 */
-	bool ListDelete(int x, ElemType &e)
-	{
-		ListNode *p, *q;
-		p = GetElem(x - 1); // 查找删除位置的前驱结点
-		if (!p || p->next == nullptr) // 删除位置不合法 或 GetElem得到尾结点
-			return false;
-
-		q = p->next;       // 令q指向被删除结点
-		p->next = q->next; // 将*q结点从链中“断开”
-		e = q->data;       // 被删除元素的值赋给e
-		free(q);           // 释放结点的存储空间
-		return true;
-	}
-
-	/* 输出操作，顺序输出线性表L的所有元素值 */
-	void PrintList()
-	{
-		ListNode *p = L->next; // 从第一个结点开始输出
-		while (p != nullptr)
-		{
-			cout << p->data << ' ';
-			p = p->next;
-		}
-		cout << endl;
-	}
-}; // List
-
-template <typename T> using ArcNode = ListNode<T>; // 边(表)结点
-
-template <typename T> struct VNode // 顶点(表)结点
-{
+/* 顶点表结点模板类 */
+template <typename VertexType, typename InfoType> struct VNode {
 	// 成员
-	T data; ArcNode *first; // 顶点信息、指向第一条依附该顶点的弧的指针
+	VertexType data;                     // 顶点信息
+	ListNode<ArcNode<InfoType>> *first;  // 指向第一条依附该顶点的弧的指针
 	// 构造函数
-	VNode() :data(0), first(nullptr) {}
+	VNode() :data(0), first(nullptr) {}; // 空则表示不存在邻接点
 };
-template <typename T> using AdjList = VNode<T>[MaxVertexNum]; // 邻接表
 
 /* 图Graph模板类（邻接表） */
-template <typename VertexType> class Graph { // VertexType：顶点的数据类型
+template <typename VertexType, typename InfoType> class Graph { // VertexType：顶点的数据类型，InfoType：带权图中"边上权值"的数据类型
 
 private:
+	using VNode = VNode<VertexType, InfoType>;
+	using ArcNode = ArcNode<InfoType>;
+
 	/* 图的邻接表存储结构 */
-	AdjList<VertexType> vertices;   // 邻接表
-	int vexnum, arcnum;             // 图的顶点数和弧数
+	vector<VNode> vertices = vector<VNode>(MaxVertexNum); // 顶点表
+	int vexnum, arcnum; // 图的顶点数和弧数
+
+	// 顶点表顺序存储，便于随机访问。边表(各个顶点的邻接表)在顶点结点中。
 
 	bool visited[MaxVertexNum]; // 访问标记数组
 
+
 	queue<int> Q; // 辅助队列Q
 
+	/* 辅助数组，对当前V－U集中的每个顶点，记录和顶点集U中顶点相连接的代价最小的边 */
+	struct Closedge { VertexType adjvex; InfoType lowcost; } closedge[MaxVertexNum];
+
 public:
-	stringstream ss; // 用于处理结果
+	queue<int> print; // 用于处理输出结果
 
-	//Graph(int Vexnum, int Edgenum) :vexnum(Vexnum), arcnum(Edgenum)
-	//{
-	//	/* 初始化 */
-	//	for (int i = 0; i < vexnum; i++) Vex[i] = i + 1; // 顶点信息从1计起
-
-	//	int x, y; // 邻接的两个顶点
-	//	for (int i = 0; i < arcnum; i++) // 输入每条边
-	//	{
-	//		cin >> x >> y;
-	//		if (Edge[x - 1][y - 1] != 0)
-	//		{
-	//			i--;
-	//			continue;
-	//		}
-	//		else
-	//		{
-	//			Edge[x - 1][y - 1] = 1;
-	//			Edge[y - 1][x - 1] = 1;
-	//		}
-	//	}
-	//}
-
-	/* 求图G中顶点x的第一个邻接点，若有则返回顶点号。若x没有邻接点或图中不存在x，则返回-1 */
-	int FirstNeighbor(int x)
+	/* (带权)无向图构造 */
+	Graph(int _vexnum, int _arcnum) :vexnum(_vexnum), arcnum(_arcnum)
 	{
-		if (x >= vexnum) return -1; // 图中不存在x，返回-1
-
+		/* 初始化顶点集、边集 */
+		for (int i = 0; i < vexnum; i++) Vex[i] = i + 1; // 顶点信息从1计起
 		for (int i = 0; i < vexnum; i++)
-			if (Edge[x][i] != 0)    // 若存在第一个邻接点，则返回顶点号
-				return i;
-		if (vertices[x])
+			for (int j = 0; j < vexnum; j++)
+				Edge[i][j] = INT_MAX/*0*/; // 带权图(无权图)
+
+		int x, y, w, index_x, index_y;     // 邻接的两个顶点、边权值
+		for (int i = 0; i < arcnum; i++)   // 输入每条边
 		{
-
+			cin >> x >> y >> w;
+			index_x = LocateVex(x); index_y = LocateVex(y);
+			Edge[index_x][index_y] = w;
+			Edge[index_y][index_x] = w;
 		}
-
-
-		return -1; // 若x没有邻接点，返回-1
 	}
 
-	/* 假设图G中顶点y是顶点x的一个邻接点，则返回除y外顶点x的下一个邻接点的顶点号。若y是x的最后一个邻接点，则返回-1 */
+	/* 按值查找。若图G中存在值为val的顶点，则返回该顶点在图中的位置，否则返回-1 */
+	int LocateVex(VertexType val)
+	{
+		for (int i = 0; i < vexnum; i++) // 顶点集中查找值为val的顶点
+			if (Vex[i] == val)
+				return i;
+		return -1; // 不存在则返回-1
+	}
+
+	/* 求图G中位置为x的顶点的第一个邻接点，若有则返回顶点号。若其没有邻接点或在图中不存在，则返回-1 */
+	int FirstNeighbor(int x)
+	{
+		if (x < 0 || x >= vexnum) return -1;  // 图中不存在位置为x的顶点，返回-1
+
+		for (int i = 0; i < vexnum; i++)      // 在边集中查找其邻接点
+			if (Edge[x][i] != INT_MAX/*0*/)   // 若存在第一个邻接点，则返回顶点号
+				return i;
+
+		return -1; // 若位置为x的顶点没有邻接点，返回-1
+	}
+
+	/* 求图G中位置为x的顶点(相对于y处的)下一个邻接点的位置。若y是x的最后一个邻接点，则返回-1 */
 	int NextNeighbor(int x, int y)
 	{
-		if (y == vexnum - 1) return -1; // 越界，y+1将会等于vexnum，则循环不会执行
-		for (int i = y + 1; i < vexnum; i++)
-			if (Edge[x][i] != 0) // 除y外顶点x的下一个邻接点
+		for (int i = y + 1; i < vexnum; i++) // 在边集中查找除y外顶点x的下一个邻接点
+			if (Edge[x][i] != INT_MAX/*0*/)  // 若存在下一个邻接点，则返回顶点号
 				return i;
 
 		return -1; // 若y是x的最后一个邻接点，则返回-1
 	}
 
-	/* 访问图中的顶点v */
-	void visit(int v) { ss << Vex[v] << " "; }
+	/* 访问图中位置为x的顶点 */
+	void visit(int x) { print.push(Vex[x]); }
 
-	/* 对图G进行深度优先遍历 */
+	/* 深度优先遍历，从第一个顶点出发 */
 	void DFSTraverse()
 	{
-		for (int v = 0; v < vexnum; v++) // 初始化访问标记数组
-			visited[v] = false;
-		for (int v = 0; v < vexnum; v++) // 本代码中是从v=0开始遍历
-			if (!visited[v]) DFS(v);
+		for (int i = 0; i < vexnum; i++) // 初始化访问标记数组
+			visited[i] = false;
+		for (int i = 0; i < vexnum; i++) // 本代码中是从v=0(第一个顶点)开始遍历
+			if (!visited[i]) DFS(i);
 	}
-	void DFSTraverse(int x)
+	/* 深度优先遍历，从给定顶点v出发 */
+	void DFSTraverse(VertexType v)
 	{
-		for (int v = 0; v < vexnum; v++) // 初始化访问标记数组
-			visited[v] = false;
-		for (int v = x; v < vexnum; v++) // 本代码中是从v=x开始遍历
-			if (!visited[v]) DFS(v);
+		for (int i = 0; i < vexnum; i++) // 初始化访问标记数组
+			visited[i] = false;
+
+		int index = LocateVex(v);        // 找到起始顶点的位置
+		DFS(index);                      // 首先深度优先遍历该顶点(所在的连通分量)
+
+		for (int i = 0; i < vexnum; i++) // 遍历其他连通分量
+			if (!visited[i]) DFS(i);
 	}
-	void DFS(int v) // 从顶点v出发，深度优先遍历图G
+	/* 从位置为v的顶点出发，递归地深度优先遍历图G */
+	void DFS(int v)
 	{
 		visit(v); visited[v] = true; // 访问顶点v
-		for (int w = FirstNeighbor(v); w >= 0; w = NextNeighbor(v, w))
-			if (!visited[w]) DFS(w); //w为v的尚未访问的邻接顶点
+		for (int w = FirstNeighbor(v); w >= 0; w = NextNeighbor(v, w)) // w为v的尚未访问的邻接点
+			if (!visited[w]) DFS(w); // 递归地遍历其邻接点(深度优先)
 	}
 
-	/* 对图G进行广度优先遍历 */
+	/* 广度优先遍历，从第一个顶点出发 */
 	void BFSTraverse()
 	{
 		for (int i = 0; i < vexnum; i++)  // 初始化访问标记数组
@@ -269,23 +138,29 @@ public:
 		Q = queue<int>();                 // 初始化辅助队列Q
 
 		for (int i = 0; i < vexnum; i++)  // 从0号顶点开始遍历
-			if (!visited[i]) BFS(i);      // 对每个连通分量调用一次BFS
+			if (!visited[i]) BFS(i);      // 对每个"连通分量"调用一次BFS
 	}
-	void BFSTraverse(int x)
+	/* 广度优先遍历，从给定顶点v出发 */
+	void BFSTraverse(VertexType v)
 	{
 		for (int i = 0; i < vexnum; i++)  // 初始化访问标记数组
 			visited[i] = false;
 
 		Q = queue<int>();                 // 初始化辅助队列Q
 
-		for (int i = x; i < vexnum; i++)  // 从x号顶点开始遍历
-			if (!visited[i]) BFS(i);      // 对每个连通分量调用一次BFS
+		int index = LocateVex(v);         // 找到起始顶点的位置
+		BFS(index);                       // 首先广度优先遍历该顶点(所在的连通分量)
+
+		for (int i = 0; i < vexnum; i++)  // 遍历其他连通分量
+			if (!visited[i]) BFS(i);
 	}
-	void BFS(int v) // 从顶点v出发，广度优先遍历图G
+	/* 从位置为v的顶点出发，非递归地广度优先遍历图G */
+	void BFS(int v)
 	{
 		visit(v); visited[v] = true; // 访问顶点v
-		Q.push(v);                   // 顶点v入队列Q
+		Q.push(v);                   // 顶点v入队列Q(以检测其所有邻接点)
 
+		/* 开始迭代访问与v有路径相通且路径长度为1,2,3...的顶点 */
 		while (!Q.empty())
 		{
 			v = Q.front(); Q.pop(); // 顶点v出队列
@@ -293,10 +168,100 @@ public:
 			{
 				if (!visited[w]) // w为v的尚未访问的邻接点
 				{
-					visit(w); visited[w] = true;
-					Q.push(w); // 顶点w入队列(下一批被访问的顶点)
+					visit(w); visited[w] = true; // 访问顶点w
+					Q.push(w); // 顶点w入队列(其邻接点为下一批被访问的顶点)
 				}
 			}
 		}
 	}
+
+	/* 在辅助数组closedge中，从V-U集中选择最小跨越边(lowcost最小)对应的顶点k，返回其位置 */
+	int minimum() // closedge[k].lowcost = min{closedge[v_i].lowcost | closedge[v_i].lowcost>0, v_i属于V-U}
+	{
+		int min_adj; EdgeType min_cost = INT_MAX; // 最小跨越边的V-U集顶点、边权值
+		for (int i = 0; i < arcnum; i++) // 遍历closedge数组
+		{
+			if (closedge[i].lowcost != 0 && closedge[i].lowcost < min_cost) // V-U集顶点中，跨越边权值最小
+			{
+				min_adj = i; // V-U集中的顶点k的位置(对应的U集顶点在closedge[i].adjvex中)
+				min_cost = closedge[i].lowcost; // 最小跨越边的权值
+			}
+		}
+		return min_adj; // 返回V-U集顶点k的位置
+	}
+
+	/* 用普里姆(Prim)算法从顶点u出发构造网G的最小生成树T，输出T的各条边。 */
+	void MiniSpanTree_Prim(VertexType u)
+	{
+		int k = LocateVex(u); int sum = 0; // 顶点u的位置、生成树权值之和
+		for (int j = 0; j < vexnum; j++)   // 辅助数组初始化
+		{
+			/* 初始时，U仅包含顶点u，即 U = {u} */
+			if (j != k) closedge[j] = { u ,Edge[k][j] }; // 此时V-U集的顶点优先级即为顶点与u邻接的边权值(不存在边则为INT_MAX)
+		}
+		closedge[k].lowcost = 0; // lowcost置为0则表示已并入U集
+
+		for (int i = 1; i < vexnum; i++) // 选择其余vexnum-1个顶点，因此需迭代vexnum-1次，每次U集并入一个顶点
+		{
+			k = minimum(); // 从V-U集中选择最小跨越边对应的顶点，它将是最小生成树T的下一个顶点
+			// 即 closedge[k].lowcost = min{closedge[v_i].lowcost | closedge[v_i].lowcost>0, v_i属于V-U}
+
+			// 输出生成树的边(最小跨越边)
+			cout << closedge[k].adjvex        // U集的顶点(此前已并入)
+				<< ' ' << Vex[k]              // V-U集的顶点
+				<< ' ' << closedge[k].lowcost // 边权值
+				<< endl;
+			sum += closedge[k].lowcost;       // 记入权值之和
+
+			closedge[k].lowcost = 0; // 新顶点k并入U集
+
+			/* 更新顶点优先级数，与顶点k互不关联的顶点都无需考虑 */
+			for (int v = 0; v < vexnum; v++) // 只需遍历顶点k的每一邻居v
+			{
+				if (Edge[k][v] < closedge[v].lowcost) // 若边kv的权值 小于 当前的优先级数(之前的其他跨越边的权值，如uv)
+				{
+					closedge[v] = { Vex[k] ,Edge[k][v] }; // 则更新顶点优先级
+				}
+			}
+		}
+		cout << sum << endl; // 最小生成树构造完成，输出总权值
+	}
 };
+
+int main()
+{
+	int n, m; cin >> n >> m;
+
+	Graph<int, int> G(n, m); // 构造图
+
+	int StartVex; cin >> StartVex;
+	if (G.LocateVex(StartVex) == -1)
+	{
+		cout << "起始顶点不存在" << endl;
+	}
+	else
+	{
+		stringstream result; cout << endl;
+
+		G.BFSTraverse(StartVex); cout << "BFS:";// 广度优先搜索
+		for (int i = 0; i < n - 1; i++)
+		{
+			cout << G.print.front() << ' ';
+			G.print.pop();
+		}
+		cout << G.print.front() << endl << endl; G.print.pop();
+
+		G.DFSTraverse(StartVex); cout << "DFS:";// 深度优先搜索
+		for (int i = 0; i < n - 1; i++)
+		{
+			cout << G.print.front() << ' ';
+			G.print.pop();
+		}
+		cout << G.print.front() << endl << endl; G.print.pop();
+
+		cout << "MST:" << endl;
+		G.MiniSpanTree_Prim(StartVex); // 最小生成树
+	}
+
+	return 0;
+}
