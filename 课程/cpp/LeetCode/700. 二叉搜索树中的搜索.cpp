e1527f725851e488895a0cd4e1652ed20514fc1e
Hazuki-295 <luosheng233@outlook.com>
2021-06-02 02:01:20 +0800

    Create 700. 二叉搜索树中的搜索.cpp

---
+++ 课程/cpp/LeetCode/700. 二叉搜索树中的搜索.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+	TreeNode *searchBST(TreeNode *root, int val) {
+		while (root != nullptr && val != root->val) { // 若树空或等于根结点值，则结束循环
+			if (val < root->val)      // 小于，则在左子树上查找
+				root = root->left;
+			else if (val > root->val) // 大于，则在右子树上查找
+				root = root->right;
+		}
+		return root;
+	}
+};
