dcdba12c64922b8226233e219263b19de4a96308
Hazuki-295 <luosheng233@outlook.com>
2021-06-13 22:45:45 +0800

    Update 912. 排序数组.cpp

--- 课程/cpp/LeetCode/912. 排序数组.cpp
+++ 课程/cpp/LeetCode/912. 排序数组.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+/* 快速排序 */
 class Solution {
 public:
 	vector<int> sortArray(vector<int> &nums) {
@@ -35,5 +36,50 @@ public:
 		nums[low] = pivot; // 枢轴元素存放到最终位置
 
 		return low; // 返回划分点
+	}
+};
+
+/* 堆排序 */
+class Solution {
+public:
+	vector<int> sortArray(vector<int> &nums) {
+		heapSort(nums);
+		return nums;
+	}
+
+	/* 建立大根堆 */
+	void buildMaxHeap(vector<int> &nums, int length) {
+		for (int i = length / 2; i >= 1; i--) { // 从最后一个非终端节点开始调整
+			heapAdjust(nums, i, length);
+		}
+	}
+	void heapAdjust(vector<int> &nums, int k, int length) { // 自堆顶k至叶子调整
+		nums[0] = nums[k]; // A[0]暂存子树的根结点(待调整元素)
+		for (int i = 2 * k; i <= length; i *= 2) {     // 沿key较大的孩子结点向下筛选
+			if (i < length && nums[i] < nums[i + 1]) { // 选择key较大的孩子结点作为下标(注意防止下标越界)
+				i = i + 1;
+			}
+			if (nums[0] >= nums[i]) { // 根结点(待调整元素)比左右孩子都大，无需调整
+				break;                // 则调整结束
+			}
+			else {
+				nums[k] = nums[i]; // 将孩子结点A[i]调整到双亲结点上
+				k = i;             // 修改k值为其孩子位置，继续向下筛选
+			}
+		}
+		nums[k] = nums[0]; // 被筛选结点的值放入最终位置
+	}
+
+	void heapSort(vector<int> &nums) {
+		int length = nums.size();
+		nums.insert(nums.begin(), 0); // 插入哨兵(使得完全二叉树满足计算式)
+
+		buildMaxHeap(nums, length);        // 无序序列建成大顶堆
+		for (int i = length; i > 1; i--) { // (调整建新堆将调用n-1次)
+			swap(nums[i], nums[1]);        // 输出堆顶元素(和堆底元素交换)
+			heapAdjust(nums, 1, i - 1);    // 调整，把剩余的i-1个元素整理成堆
+		}
+
+		nums.erase(nums.begin()); // 完成后移除哨兵
 	}
 };
