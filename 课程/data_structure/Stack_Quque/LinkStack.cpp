f0c3e7452a8fec2933080433a825a921fd2bd89f
Hazuki-295 <luosheng233@outlook.com>
2021-06-19 14:37:53 +0800

    Update LinkStack.cpp

--- 课程/data_structure/Stack_Quque/LinkStack.cpp
+++ 课程/data_structure/Stack_Quque/LinkStack.cpp
@@ -4,11 +4,9 @@ using namespace std;
 #include "../List/LinkList.cpp"
 
 /* 以列表为基类，派生出栈模板类 */
-template <typename T> class Stack : public List<T> { //将列表的首/末端作为栈顶/底
-
+template <typename T> class Stack : public List<T> { // 将列表的首/末端作为栈顶/底
 private:
 	typedef T ElemType;
-
 public:
 	// 构造函数
 	Stack() { InitStack(); } //默认
@@ -20,57 +18,48 @@ public:
 	bool StackEmpty() { return List<T>::LinkLength() == 0; }
 
 	/* 进栈，将x加入使之成为新的栈顶 */
-	bool Push(ElemType x)
-	{
-		return List<T>::ListInsert(List<T>::LinkLength() + 1, x);
+	bool Push(ElemType x) {
+		return List<T>::ListInsert(List<T>::LinkLength() + 1, x); // 在表尾插入
 	}
 
 	/* 出栈，弹出栈顶元素，并用x返回其值 */
-	bool Pop(ElemType &x)
-	{
-		return List<T>::ListDelete(List<T>::LinkLength(), x);
+	bool Pop(ElemType &x) {
+		return List<T>::ListDelete(List<T>::LinkLength(), x); // 表尾元素删除
+	}
+
+	/* 读栈顶元素，若栈非空，则用x返回栈顶元素 */
+	bool GetTop(ElemType &x) {
+		auto p = List<T>::GetElem(List<T>::LinkLength()); // 取表尾元素
+		if (p != nullptr) {
+			x = p->data;
+			return true;
+		}
+		return false;
 	}
 }; // Stack
 
-int main()
-{
-	int n, temp;
+int main() {
+	Stack<int> S; S.InitStack();
 
-	Stack<int> L;
+	cout << "请输入入栈元素个数：";
+	int num; cin >> num;
 
-	cout << "请输入链表结点个数：\n";
-	cin >> n; cout << '\n';
-
-	cout << "请输入" << n << "个链表元素：\n";
-	L.List_TailInsert(n); //初始化链表
-
-
-	cout << "\n初始化完成。\n";
-	/* 测试代码 */
-	cout << "当前链表长度为：\n" << L.LinkLength() << '\n';
-	cout << "当前链表元素为：\n"; L.PrintList(); cout << "\n\n";
-
+	cout << "请输入" << num << "个入栈元素：";
 	int delete1, insert1;
-
-	while (true)
-	{
-		/* 入栈测试 */
-		cout << "入栈：\n";
+	for (int i = 0; i < num; i++) {
 		cin >> insert1;
-		L.Push(insert1);
-		/* 测试代码 */
-		cout << "当前链表长度为：\n" << L.LinkLength() << '\n';
-		cout << "当前链表元素为：\n"; L.PrintList(); cout << "\n\n";
+		S.Push(insert1);
 	}
 
-	while (!L.StackEmpty())
-	{
-		/* 出栈测试 */
-		cout << "出栈。\n";
-		L.Pop(delete1);
-		/* 测试代码 */
-		cout << "当前链表长度为：\n" << L.LinkLength() << '\n';
-		cout << "当前链表元素为：\n"; L.PrintList(); cout << "\n\n";
-	}
+	/* 取栈顶 */
+	int top; S.GetTop(top);
+	cout << "栈顶元素：" << top << endl;
 
+	/* 出栈测试 */
+	cout << "出栈元素：\n";
+	while (!S.StackEmpty()) {
+		S.Pop(delete1);
+		cout << delete1 << " ";
+	}
+	cout << endl;
 }
