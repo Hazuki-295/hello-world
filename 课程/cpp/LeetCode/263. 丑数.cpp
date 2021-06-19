309427015278f26bbbf6578a410968fae125153d
Hazuki-295 <luosheng233@outlook.com>
2021-06-19 16:06:29 +0800

    Update 263. 丑数.cpp

--- 课程/cpp/LeetCode/263. 丑数.cpp
+++ 课程/cpp/LeetCode/263. 丑数.cpp
@@ -7,8 +7,7 @@ public:
 	bool isUgly(int n) {
 		if (n <= 0) return false;
 		vector<int> factors = { 2,3,5 }; // 只含有质因子2,3,5
-		for (auto factor : factors)
-		{
+		for (auto factor : factors) {
 			while (n % factor == 0) // 除尽所含的2,3,5因子
 				n /= factor;
 		}
