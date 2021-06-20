fbf65a9b53e31b29412fda9ed3a5d1dd2248a8a0
Hazuki-295 <luosheng233@outlook.com>
2021-06-20 15:24:12 +0800

    Update BiTree.cpp

--- 课程/data_structure/BiTree/BiTree.cpp
+++ 课程/data_structure/BiTree/BiTree.cpp
@@ -3,21 +3,18 @@
 #include <queue>
 using namespace std;
 
-#define MaxSize 50
-
 /* 二叉树的二叉链表存储表示 */
-template<typename T> struct BiTNode
-{
+template<typename T> struct BiTNode {
 	// 成员
 	T data; BiTNode *lchild, *rchild; // 值、左右孩子指针
 	// 构造函数
+	BiTNode() :data(T()), lchild(nullptr), rchild(nullptr) {}
 	BiTNode(T x) :data(x), lchild(nullptr), rchild(nullptr) {}
 };
 template<typename T> using BiTree = BiTNode<T> *;
 
 /* 二叉树模板类 */
-template<typename T> class BinTree
-{
+template<typename T> class BinTree {
 private:
 	using ElemType = T;
 	using BiTNode = BiTNode<T>;
@@ -40,16 +37,13 @@ public:
 
 
 	/* 按先序扩展序列(字符)，建立二叉树的二叉链表 */
-	void CreateBiTree(BiTree &T)
-	{
+	void CreateBiTree(BiTree &T) {
 		char c;
-		if (cin >> c)
-		{
+		if (cin >> c) {
 			if (c == '#')     // '#'表示空树
 				T = nullptr;
-			else
-			{
-				T = new BiTNode(c);      // 生成根结点  
+			else {
+				T = new BiTNode(c);      // 生成根结点
 				CreateBiTree(T->lchild); // 递归构造左子树
 				CreateBiTree(T->rchild); // 递归构造右子树
 			}
@@ -60,22 +54,19 @@ public:
 	static void PrintElement(ElemType &e) { cout << e; }
 
 	/* 统计二叉树中叶子结点的个数 */
-	void CountLeaf(BiTree T, int &count)
-	{
-		if (T != nullptr)
-		{
-			if ((!T->lchild) && (!T->rchild)) // 如果是叶子结点
+	void CountLeaf(BiTree T, int &count) {
+		if (T != nullptr) {
+			if ((!T->lchild) && (!T->rchild)) { // 如果是叶子结点
 				count++; // 对叶子结点计数
+			}
 			CountLeaf(T->lchild, count);
 			CountLeaf(T->rchild, count);
 		}
 	}
 
 	/* 统计二叉树中结点的个数 */
-	void CountNode(BiTree T, int &count)
-	{
-		if (T != nullptr)
-		{
+	void CountNode(BiTree T, int &count) {
+		if (T != nullptr) {
 			//if ((!T->lchild) && (!T->rchild)) // 如果是叶子结点
 			count++; // 对叶子结点计数
 			CountNode(T->lchild, count);
@@ -84,13 +75,12 @@ public:
 	}
 
 	/* 求二叉树的深度 */
-	int Depth(BiTree T)
-	{
+	int Depth(BiTree T) {
 		int depthval, depthLeft, depthRight;
-		if (T == nullptr)
+		if (T == nullptr) {
 			depthval = 0;
-		else
-		{
+		}
+		else {
 			depthLeft = Depth(T->lchild);   // 递归求左子树深
 			depthRight = Depth(T->rchild);  // 递归求右子树深
 			depthval = 1 + (depthLeft > depthRight ? depthLeft : depthRight); // 树根深度，为左右子树的最大深度+1
@@ -99,18 +89,16 @@ public:
 	}
 
 	/* 求二叉树的深度 */
-	int maxDepth(BiTNode *root)
-	{
-		if (root == nullptr)
+	int maxDepth(BiTNode *root) {
+		if (root == nullptr) {
 			return 0;
+		}
 		return 1 + max(maxDepth(root->lchild), maxDepth(root->rchild));
 	}
 
 	/* 先序遍历二叉树 */
-	void PreOrder(BiTree T)
-	{
-		if (T != nullptr)
-		{
+	void PreOrder(BiTree T) {
+		if (T != nullptr) {
 			visit(T->data);      // 访问根结点
 			PreOrder(T->lchild); // 遍历左子树
 			PreOrder(T->rchild); // 遍历右子树
@@ -118,10 +106,8 @@ public:
 	}
 
 	/* 中序遍历二叉树 */
-	void InOrder(BiTree T)
-	{
-		if (T != nullptr)
-		{
+	void InOrder(BiTree T) {
+		if (T != nullptr) {
 			InOrder(T->lchild); // 遍历左子树
 			visit(T->data);     // 访问根结点
 			InOrder(T->rchild); // 遍历右子树
@@ -129,10 +115,8 @@ public:
 	}
 
 	/* 后序遍历二叉树 */
-	void PostOrder(BiTree T)
-	{
-		if (T != nullptr)
-		{
+	void PostOrder(BiTree T) {
+		if (T != nullptr) {
 			PostOrder(T->lchild); // 遍历左子树
 			PostOrder(T->rchild); // 遍历右子树
 			visit(T->data);       // 访问根结点
@@ -141,18 +125,14 @@ public:
 
 
 	/* 中序遍历二叉树(非递归算法) */
-	void InOrder2()
-	{
+	void InOrder2(BiTree T) {
 		stack<BiTree> S; BiTree p = T; // 初始化栈S; p是遍历指针
-		while (p || !S.empty()) // 栈不空(存在待访问结点) 或 p不空(中序遍历的树不是空树) 时循环
-		{
-			if (p) // 一路向左 (或 右子树不空，中序遍历之)
-			{
+		while (p || !S.empty()) { // 栈不空(存在待访问结点) 或 p不空(中序遍历的树不是空树) 时循环
+			if (p) { // 一路向左 (或 右子树不空，中序遍历之)
 				S.push(p);     // 当前结点入栈
 				p = p->lchild; // 左孩子不空，一直向左走
 			}
-			else // 出栈，并转向出栈结点的右子树
-			{
+			else { // 出栈，并转向出栈结点的右子树
 				p = S.top(); S.pop(); // 栈顶元素出栈
 				visit(p->data);       // 访问出栈结点
 				p = p->rchild;        // 向右子树走，p赋值为当前结点的右孩子
@@ -162,18 +142,14 @@ public:
 	}
 
 	/* 先序遍历二叉树(非递归算法) */
-	void PreOrder2()
-	{
+	void PreOrder2(BiTree T) {
 		stack<BiTree> S; BiTree p = T; // 初始化栈S; p是遍历指针
-		while (p || !S.empty()) // 栈不空(存在待访问结点) 或 p不空(先序遍历的树不是空树) 时循环
-		{
-			if (p) // 一路向左 (或 右子树不空，先序遍历之)
-			{
+		while (p || !S.empty()) { // 栈不空(存在待访问结点) 或 p不空(先序遍历的树不是空树) 时循环
+			if (p) { // 一路向左 (或 右子树不空，先序遍历之)
 				visit(p->data); S.push(p); // 访问当前结点，并入栈
 				p = p->lchild;             // 左孩子不空，一直向左走
 			}
-			else // 出栈，并转向出栈结点的右子树
-			{
+			else { // 出栈，并转向出栈结点的右子树
 				p = S.top(); S.pop(); // 栈顶元素出栈
 				p = p->rchild;        // 向右子树走，p赋值为当前结点的右孩子
 									  // 若右孩子为空，该出栈结点遍历完毕，则继续出栈
@@ -182,13 +158,11 @@ public:
 	}
 
 	/* 层次遍历二叉树 */
-	void LevelOrder()
-	{
+	void LevelOrder(BiTree T) {
 		queue<BiTree> Q; BiTree p = T; // 初始化辅助队列; p是遍历指针
 
 		Q.push(T); // 将根结点入队
-		while (!Q.empty()) // 队列不空则循环
-		{
+		while (!Q.empty()) { // 队列不空则循环
 			p = Q.front(); Q.pop(); // 队头结点出队
 			visit(p->data); // 访问出队结点
 
@@ -200,8 +174,7 @@ public:
 	}
 };
 
-int main()
-{
+int main() {
 	BinTree<char> T;
 	T.CreateBiTree();
 
