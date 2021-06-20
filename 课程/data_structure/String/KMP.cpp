b0ed9f9b13e30a1bf17c71b17fa1c0c7308eb3de
Hazuki-295 <luosheng233@outlook.com>
2021-06-20 19:07:57 +0800

    Update KMP.cpp

--- 课程/data_structure/String/KMP.cpp
+++ 课程/data_structure/String/KMP.cpp
@@ -3,52 +3,64 @@
 #include <vector>
 using namespace std;
 
-void getNext(string T, vector<int> &next) {
-	int size = T.size();
-	T.insert(T.begin(), ' ');
-	next.resize(size + 1);
+void buildNext(string T, vector<int> &next) {
+	int length = T.size();
 
-	int i = 1, j = 0;
-	next[1] = 0;
-	while (i < size) {
-		if (j == 0 || T[i] == T[j]) {
-			i++; j++;
-			next[i] = j;
+	next.resize(length + 1);    // 串及next数组的序号均为1~n
+	T.insert(T.begin(), ' ');
+
+	int j = 0;   // "主"串指针
+	int i = 1;   // 模式串指针
+	next[1] = 0; // next[1]设置为0，0号字符为通配符
+
+	while (i < length) { // 需求出数组其余n-1项
+		if (j == 0 || T[i] == T[j]) { // 匹配
+			i++; j++;    // 下一匹配位置
+			next[i] = j; // 一旦发现P[j]与P[i]匹配(含P[j = 0]的通配)，则可令 next[j+1](新的一项)=next[j](else分支得到)+1
 		}
 		else {
-			j = next[j];
+			j = next[j]; // 失配
 		}
 	}
 }
 
-void getNextval(string T, vector<int> &nextval) {
-	int size = T.size();
-	T.insert(T.begin(), ' ');
-	nextval.resize(size + 1);
+void buildNextval(string T, vector<int> &nextval) {
+	int length = T.size();
 
-	int i = 1, j = 0;
-	nextval[1] = 0;
-	while (i < size) {
-		if (j == 0 || T[i] == T[j]) {
+	nextval.resize(length + 1);    // 串及next数组的序号均为1~n
+	T.insert(T.begin(), ' ');
+
+	int j = 0;   // "主"串指针
+	int i = 1;   // 模式串指针
+	nextval[1] = 0; // next[1]设置为0，0号字符为通配符
+
+	while (i < length) { // 需求出数组其余n-1项
+		if (j == 0 || T[i] == T[j]) { // 匹配
 			i++; j++;
-			nextval[i] = (T[i] != T[j] ? j : nextval[j]);
+			nextval[i] = (T[i] != T[j] ? j : nextval[j]); // 吸取教训
 		}
-		else {
+		else { // 失配
 			j = nextval[j];
 		}
 	}
 }
 
 int main() {
-	string test = "abaabcaba";
-	vector<int> next, nextval;
-	getNext(test, next);
-	getNextval(test, nextval);
 
+	cout << "请输入字符串：";
+	string test; cin >> test; // ababcaab
+
+	vector<int> next, nextval;
+	buildNext(test, next);
+	buildNextval(test, nextval);
+
+	cout << "next数组：\t";
 	for (size_t i = 1; i < next.size(); i++) {
 		cout << next[i] << " ";
 	}
 	cout << endl;
+
+	cout << "nextval数组：\t";
 	for (size_t i = 1; i < nextval.size(); i++) {
 		cout << nextval[i] << " ";
 	}
