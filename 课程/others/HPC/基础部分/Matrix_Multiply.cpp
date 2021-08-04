e1804c965aa3cbd5d92d3413f158d0e26fa30315
Hazuki-295 <luosheng233@outlook.com>
2021-08-04 21:21:32 +0800

    Update Matrix_Multiply.cpp

--- 课程/others/HPC/基础部分/Matrix_Multiply.cpp
+++ 课程/others/HPC/基础部分/Matrix_Multiply.cpp
@@ -7,30 +7,32 @@
 #define N 1024
 
 /* 矩阵分块乘法 */
-void multiply_tiled(double *a, double *b, double *c) 
-{
+void multiply_tiled(double *a, double *b, double *c) {
 	/* Loop over all the tiles, stride by tile size */
 	/* 迭代遍历所有的块，按块的大小前进 */
 	int i, j, k, x, y, z;
 	double sum;
-	for (i = 0; i < N; i += TILE)
-		for (j = 0; j < N; j += TILE)
-			for (k = 0; k < N; k += TILE)
+	for (i = 0; i < N; i += TILE) {
+		for (j = 0; j < N; j += TILE) {
+			for (k = 0; k < N; k += TILE) {
 				/* Regular multiply inside the tiles */
 				/* 每个分块内进行常规的矩阵乘法 */
-				for (x = i; x < i + TILE; x++)
+				for (x = i; x < i + TILE; x++) {
 					for (y = j; y < j + TILE; y++) {
 						sum = c[x * N + y];
-						for (z = k; z < k + TILE; z++) 
-						{
+						for (z = k; z < k + TILE; z++) {
 							sum += a[x * N + z] * b[z * N + y];
 						}
 						c[x * N + y] = sum;
 					}
+				}
+			}
+		}
+	}
+		
 }
 
-int Divided_Matrix_Multiply()
-{
+int Divided_Matrix_Multiply() {
 	double *a, *b, *c;
 	double sum;
 	a = (double *)malloc(N * N * sizeof(double));
@@ -38,15 +40,21 @@ int Divided_Matrix_Multiply()
 	c = (double *)malloc(N * N * sizeof(double));
 
 	/* 初始化矩阵 */
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			a[i * N + j] = 1.0;
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			b[i * N + j] = 1.0;
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			c[i * N + j] = 0;
+		}	
+	}
 
 	time_t plainstart = time(NULL);
 
@@ -60,12 +68,13 @@ int Divided_Matrix_Multiply()
 			c[i * N + j] = sum;
 		}
 	time_t plainend = time(NULL);
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-		{
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			// printf("c:%d",(int)c[i*N+j]);
 			c[i * N + j] = 0;
 		}
+	}
+		
 	time_t optstart = time(NULL);
 	multiply_tiled(a, b, c);
 	time_t optend = time(NULL);
