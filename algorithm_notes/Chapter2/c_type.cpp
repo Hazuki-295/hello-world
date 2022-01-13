64a7a24af9bc1e7ddb868ead2c3b95d5b5af384c
Hazuki-295 <luosheng233@outlook.com>
2022-01-13 20:10:45 +0800

    Update c_type.cpp

--- algorithm_notes/Chapter2/c_type.cpp
+++ algorithm_notes/Chapter2/c_type.cpp
@@ -4,14 +4,17 @@ using namespace std;
 
 int main() {
     int maxNum = INT_MAX;
-    long biggerNum = LONG_MAX;
+    long biggerNum = LONG_MAX; // 随操作系统不同。Ubuntu 64 下为 8bytes，Windows 下为 4bytes
     long long bigNum1 = LLONG_MAX;
-    long long bigNum2 = 123456789012345;
+    long long bigNum2 = 123456789012345; // 字面值类型也随之改变
 
     cout << maxNum << "\n"
          << biggerNum << "\n"
          << bigNum1 << "\n"
          << bigNum2 << endl;
+
+    bool boolTest = 5;
+    printf("%d\n", boolTest); // 将整型值赋值给bool类型，自动转换成0或1。
 
     return 0;
 }
