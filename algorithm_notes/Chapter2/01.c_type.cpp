fc526d94bc13b24932efa12adc33c9b8da0dbf8b
Hazuki-295 <luosheng233@outlook.com>
2022-02-24 00:04:47 +0800

    Update 01.c_type.cpp

--- algorithm_notes/Chapter2/01.c_type.cpp
+++ algorithm_notes/Chapter2/01.c_type.cpp
@@ -3,18 +3,25 @@
 using namespace std;
 
 int main() {
-    int num = INT_MAX;
-    long varNum = LONG_MAX; // 随操作系统不同。Ubuntu 64 下为 8bytes，Windows 下为 4bytes
-    long long bigNum1 = LLONG_MAX;
-    long long bigNum2 = 123456789012345; // 字面值类型也随之改变
+    /* 整型 */
+    int num = INT_MAX;                    /* 数量级：10^9 */
+    long varNum = LONG_MAX;               // 随操作系统不同。Ubuntu 64 下为 8bytes，Windows 下为 4bytes
+    long long bigNum1 = LLONG_MAX;        /* 数量级：10^18 */
+    long long bigNum2 = 123456789012345;  // 字面值的类型也随之改变
 
-    cout << num << "\n"
+    cout << "整型：\n"
+         << num << "\n"
          << varNum << "\n"
          << bigNum1 << "\n"
          << bigNum2 << endl;
 
-    bool boolTest = 5;
-    printf("%d\n", boolTest); // 将整型值赋值给bool类型，自动转换成0或1。
+    bool boolTest = 5;         // (隐式类型转换) 将整型值赋值给bool类型，自动转换成0或1。
+    printf("%d\n", boolTest);  // 当然，在实际使用时，直接给布尔变量赋值 true 或 false 就行。
+
+    /* 浮点型 */
+    float fl = 3.1415;         // 有效精度：6～7 位
+    double db = 3.1415926536;  // 有效精度：15～16 位
+    printf("浮点型：%f, %f\n", fl, db);  // 不指定时，默认显示6位小数
 
     return 0;
 }
