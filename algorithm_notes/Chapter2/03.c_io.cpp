c590a41ddf4dcc27353687c14e4ad241ffa39665
Hazuki-295 <luosheng233@outlook.com>
2022-03-01 00:22:58 +0800

    Update 03.c_io.cpp

--- algorithm_notes/Chapter2/03.c_io.cpp
+++ algorithm_notes/Chapter2/03.c_io.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 
 int main() {
-    /* scanf() function, conversion specifications */
+    /* Examples for the scanf() function - Format specifier */
     int num;
     long long bigNum;
     scanf("%d, %lld;", &num, &bigNum);  // long long 使用 %lld
 
-    float f;
-    double d;
-    scanf("%f, %lf;", &f, &d);  // 在 scanf 中 double 使用 %lf
+    float fl;
+    double db;
+    scanf("%f, %lf;", &fl, &db);  // 在 scanf 中 double 使用 %lf
 
-    char c;                     // 注意：%c 将会读入空白符(空格、换行等)，而其他格式描述符(如 %d)的输入则是以空白符为结束判断标志的。
-    char str[15];               // 因此，除非使用 %c 把空格按字符读入，其他情况都会自动跳过空格。
-    scanf("%c, %s.", &c, str);  // 另外，str 为数组名，本身就是地址。
+    char c;
+    char str[15];
+    scanf("%c, %s.", &c, str);  // str 为数组名，本身就是地址。
 
-    /* "2147483647, 9223372036854775807; 3.1415, 3.1415926536; , Hello,World! ." */
+    char space, new_line;
+    scanf("%c%c", &space, &new_line);
+
+    /***********************************************************************************
+     * 注意：
+     * 1. %c 将会读入空白符(空格、换行等)，而其他格式描述符(如 %d)的输入则是以空白符为结束判断标志的。
+     *      因此，除非使用 %c 把空格按字符读入，其他情况都会自动跳过空格。
+     * 2. 输入示例："2147483647, 9223372036854775807; 3.1415, 3.1415926536; , Hello,World!. "
+     *      最后读入的两个字符，并没有在输入时阻塞，而是直接读取了缓冲区中已存在的字符。
+     ***********************************************************************************/
 
     return 0;
 }
