897e8fb0a165376814e479d535b6dd62592cb54d
Hazuki-295 <luosheng233@outlook.com>
2022-03-02 19:52:24 +0800

    Update 05.array.cpp

--- algorithm_notes/Chapter2/05.array.cpp
+++ algorithm_notes/Chapter2/05.array.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-//int a[3000000];
+int a[3000000];
 // 如果数组的规模较大(megabyte 数量级，10^6 bytes)：
 // 1. 局部变量：如放在 main 函数中，是从”调用栈“申请空间，允许申请的空间较小，会导致程序异常退出。
 // 2. 全局变量：编译阶段，在”数据段(data segment)“中分配空间，允许申请的空间较大。
 
 int main() {
-    int myArray[500000] = {};  // all elements 0 in C++
+    int myArray[10] = {};  // all elements 0 in C++
     for (int i = 0; i < 10; i++) {
         printf("myArray[%d] = %d\n", i, myArray[i]);
     }
