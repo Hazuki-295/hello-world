c0248bd0211593566e17865ce7150da3b03f78be
Hazuki-295 <luosheng233@outlook.com>
2022-03-03 10:07:19 +0800

    Update 06.string.cpp

--- algorithm_notes/Chapter2/06.string.cpp
+++ algorithm_notes/Chapter2/06.string.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 using namespace std;
 
-// const int arraySize = 10;
-
 int main() {
-    int arraySize;
-    cin >> arraySize;
+    int myArray[5] = {};
 
-    int myArray[arraySize] = {};  // all elements 0 in C++
-    for (int i = 0; i < arraySize; i++) {
-        printf("myArray[%d] = %d\n", i, myArray[i]);
+    /* memset - fill memory with a constant byte (interpreted as an unsigned char) */
+    for (auto byte : vector<int>({0, -1, 1, 255})) {
+        memset(myArray, byte, sizeof(myArray));
+
+        printf("memset(byte: %3d) = ", byte);  // 0的补码为“全0”，-1的补码为“全1”，
+        for (auto x : myArray) {               // 因此 memset 可用来将 int 数组的所有元素赋值为0或-1
+            printf("%10d ", x);
+        }
+        printf("\n");
     }
 
     return 0;
