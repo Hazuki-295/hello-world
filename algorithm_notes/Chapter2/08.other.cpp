5aa7ec596ad15e0f8a8e69754afb1bfa8b5c91e0
Hazuki-295 <luosheng233@outlook.com>
2022-03-05 23:17:58 +0800

    Update 08.other.cpp

--- algorithm_notes/Chapter2/08.other.cpp
+++ algorithm_notes/Chapter2/08.other.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 using namespace std;
 
-const double eps = 1e-8;       // 极小数eps
+const double eps = 1e-8;       // 极小数ε - Epsilon
 const double pi = acos(-1.0);  // 圆周率
 
 #define Equ(a, b) ((fabs((a) - (b))) < (eps))  // ==
