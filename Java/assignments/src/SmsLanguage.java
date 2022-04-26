dc38d736fb296223dbb1033e490cb857ea84f21a
Hazuki-295 <luosheng233@outlook.com>
2022-04-26 18:22:00 +0800

    Create SmsLanguage.java

---
+++ Java/assignments/src/SmsLanguage.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SmsLanguage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Get test cases.
+        String str = input.nextLine();
+        int t = Integer.parseInt(str);
+
+        while (t-- > 0) {
+            String message = input.nextLine();
+
+            message = message.replaceAll("[.,?!]", "");
+            message = message.toLowerCase();
+            message = message.replace("and", "&").replace("ate", "8")
+                    .replace("at", "@").replace("you", "U");
+
+            System.out.println(message);
+        }
+    }
+}
