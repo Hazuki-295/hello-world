4563e8c77af77d5933567ae815426e7ffa5f357f
Hazuki-295 <luosheng233@outlook.com>
2022-06-08 20:38:10 +0800

    Update isNumericString.java

--- Java/assignments/src/isNumericString.java
+++ Java/assignments/src/isNumericString.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class isNumeric {
+public class isNumericString {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
