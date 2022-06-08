bcbe49c1eb55451a2cb97e2726e18247137e5c7d
Hazuki-295 <luosheng233@outlook.com>
2022-06-08 20:47:55 +0800

    Update isNumericString.java

--- Java/assignments/src/isNumericString.java
+++ Java/assignments/src/isNumericString.java
@@ -6,12 +6,16 @@ public class isNumericString {
 
         while (true) {
             System.out.print("Enter a numeric number: ");
-            String line = input.nextLine();
-            System.out.println("Is it matches the token? : " + isNumeric(line));
+            if (input.hasNextLine()) {
+                String line = input.nextLine();
+                System.out.println("Is it matches the token? : " + isNumeric(line));
+            } else break;
         }
     }
 
-    /** Match a number with optional '-' and decimal. */
+    /**
+     * Match a number with optional '-' and decimal.
+     */
     public static boolean isNumeric(String str) {
         return str.matches("-?\\d+(\\.\\d+)?");
     }
