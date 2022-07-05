c54907b018c1ea2bf72bb4afd65a517ed4a6fbbf
Hazuki-295 <luosheng233@outlook.com>
2022-07-05 13:42:48 +0800

    Update Vector.h

--- 01.Data Structure/01.List/cpp version/Vector.h
+++ 01.Data Structure/01.List/cpp version/Vector.h
@@ -213,10 +213,10 @@ void Vector<T>::bubbleSort(Rank lo, Rank hi) { // assert: 0 <= lo < hi <= size
 }
 
 template<typename T>
-bool Vector<T>::bubble(Rank lo, Rank hi) {
+bool Vector<T>::bubble(Rank lo, Rank hi) { // 一趟扫描交换
     bool sorted = true; // 整体有序标志
     while (++lo < hi) { // 自左向右，逐一检查各对相邻元素
-        if (_elem[lo - 1] < _elem[lo]) {
+        if (_elem[lo - 1] > _elem[lo]) {
             sorted = false; // 若逆序，则意味着尚未整体有序
             std::swap(_elem[lo - 1], _elem[lo]); // 并需要通过交换使局部有序
         }
