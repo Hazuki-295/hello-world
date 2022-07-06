3d4a1ec4bed62abe5533ee0bdd5e2eb08b95b966
Hazuki-295 <luosheng233@outlook.com>
2022-07-06 18:36:41 +0800

    Update List.h
    implement find(), operator[]

--- 01.Data Structure/01.List/cpp version/List.h
+++ 01.Data Structure/01.List/cpp version/List.h
@@ -20,6 +20,17 @@ public:
     List(ListNodePosi<T> p, int n); // 复制列表中自位置p起的n项
 // 析构函数
     ~List(); // 释放（包括头、尾哨兵在内的）所有节点
+// 只读访问接口
+    Rank size() const { return _size; }       // 规模
+    bool empty() const { return _size <= 0; } // 判空
+    ListNodePosi<T> first() const { return header->succ; } // 首节点位置
+    ListNodePosi<T> last() const { return trailer->pred; } // 末节点位置
+    /* 运算符重载 */
+    T &operator[](Rank r) const; // 重载，支持循秩访问（效率低）
+    /* 查找接口 */
+    bool valid(ListNodePosi<T> p) { return p && trailer != p && header != p; } // 判断位置p是否对外合法（将头、尾节点等同与nullptr）
+    ListNodePosi<T> find(T const &e) const { return find(e, _size, trailer); } // 无序列表整体查找
+    ListNodePosi<T> find(T const &e, int n, ListNodePosi<T> p) const; // 无序列表区间查找
 };
 
 /* 列表初始化，在创建列表对象时统一调用。 */
@@ -28,10 +39,29 @@ void List<T>::init() {
     header = new ListNode<T>;  // 创建头哨兵节点
     trailer = new ListNode<T>; // 创建尾哨兵节点
 
-    header->succ = trailer; // 互联，头哨兵始终无前驱
-    header->pred = nullptr;
-    trailer->pred = header; // 互联，尾哨兵始终无后继
-    trailer->succ = nullptr;
+    header->succ = trailer;
+    header->pred = nullptr;  // 互联，头哨兵始终无前驱
+    trailer->pred = header;
+    trailer->succ = nullptr; // 互联，尾哨兵始终无后继
 
     _size = 0; // 初始规模为0
 }
+
+/* 重载下标运算符，以通过秩直接访问列表节点（虽方便，效率低，需慎用） */
+template<typename T>
+T &List<T>::operator[](Rank r) const { // assert: 0 <= r < _size
+    ListNodePosi<T> p = first(); // 从首节点出发
+    while (r-- > 0) { // 顺数第r个节点即是目标节点
+        p = p->succ;
+    }
+    return p->data; // 返回其中所存元素
+}
+
+/* 无序列表区间查找：在节点p的n个前驱中，找到等于e的最后者。 */
+template<typename T>
+ListNodePosi<T> List<T>::find(T const &e, int n, ListNodePosi<T> p) const {
+    while (n-- > 0) { // 对于p的最近的n个前驱，自右向左
+        if ((p->pred)->data == e) return p; // 逐个比对，直至命中
+    }
+    return nullptr; // 或p越出左边界，意味着区间内不包含e，查找失败
+} // 注：节点p可能是trailer，n个前驱指的是真前驱（哨兵节点并无数据域）
