38fb883f6cb50f2b8ddf4e4732f5743630f18bf7
Hazuki-295 <luosheng233@outlook.com>
2022-07-07 23:46:05 +0800

    Update List.h
    implement uniquify()

--- 01.Data Structure/01.List/cpp version/List.h
+++ 01.Data Structure/01.List/cpp version/List.h
@@ -42,6 +42,7 @@ public:
     T remove(ListNodePosi<T> p); // 删除合法位置p处的节点，返回被删除节点的数据项
     /* 去重 */
     int deduplicate(); // 无序去重
+    int uniquify();    // 有序去重
 // 遍历
     void traverse(void(*visit)(T &)); // 遍历（使用函数指针，只读或局部性修改）
     template<typename VST> void traverse(VST &visit); // 遍历（使用函数对象，可全局性修改）
@@ -138,7 +139,18 @@ int List<T>::deduplicate() {
         ListNodePosi<T> q = find(p->data, r, p);  // 在p的r个真前驱中查找雷同者（至多一个）
         q ? remove(q) : r++; // 注意：r为无重前缀的长度，p不断地指向下一节点
     }
-    return oldSize - _size; // 向量规模变化量，即被删除元素总数
+    return oldSize - _size; // 列表规模变化量，即被删除元素总数
+}
+
+template<typename T>
+int List<T>::uniquify() {
+    if (_size < 2) return 0; // 平凡列表自然无重复
+    int oldSize = _size;     // 记录原规模
+    ListNodePosi<T> p = first(), q; // 双指针，p为各区段起点，q则为其后继
+    while ((q = p->succ) != trailer) { // 反复考察相邻的节点对(p, q)
+        (p->data == q->data) ? remove(q) : p = q;
+    } // 若雷同，删除后者；否则（互异），转向下一区段
+    return oldSize - _size; // 列表规模变化量，即被删除元素总数
 }
 
 template<typename T>
