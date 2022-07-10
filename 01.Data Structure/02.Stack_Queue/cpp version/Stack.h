8097cbfd15626c7da414760281a3d7b2d22d3edd
Hazuki-295 <luosheng233@outlook.com>
2022-07-11 00:51:20 +0800

    Create Stack.h

---
+++ 01.Data Structure/02.Stack_Queue/cpp version/Stack.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../../01.List/cpp version/Vector.h"
+
+/* 栈模板类：利用C++的继承机制，将栈作为向量的派生类，实现栈结构。 */
+template<typename T> class Stack : private Vector<T> { // 将向量的首端作为栈底，末端作为栈顶
+public: // size()、empty()以及其他开放接口，均可直接沿用
+    using Vector<T>::size, Vector<T>::empty;
+private:
+    using Vector<T>::insert, Vector<T>::remove;
+public:
+    void push(T const &e) { insert(e); }     // 入栈：将e插至栈顶，等效于将新元素作为向量的末元素插入
+    T pop() { return remove(size() - 1); }   // 出栈：删除栈顶对象，等效于删除向量的末元素
+    T &top() { return (*this)[size() - 1]; } // 取顶：引用栈顶对象，直接返回向量的末元素
+};
