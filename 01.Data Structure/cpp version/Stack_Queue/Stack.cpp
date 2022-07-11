c5f1de7f7eedf8eb9c78ff9d00682dab6686252a
Hazuki-295 <luosheng233@outlook.com>
2022-07-11 18:04:40 +0800

    Update Stack.cpp

--- 01.Data Structure/cpp version/Stack_Queue/Stack.cpp
+++ 01.Data Structure/cpp version/Stack_Queue/Stack.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "Stack.h"
 
 using namespace std;
@@ -18,10 +19,31 @@ void print(Stack<T> &S) { // 打印栈中元素
     cout << S;
 }
 
-int main() {
-    using T = int;
-    Stack<T> myStack;
+/* 进制转换：将十进制数n，转换为base进制。 */
+string convert(size_t n, int base) {
+    string digit = "0123456789ABCDEF"; // 数位符号，如有必要可相应扩充
+    Stack<char> S;  // 辅助栈，逆序计算输出
+    while (n > 0) { // 在尚有余数之前，由低到高，逐一计算出新进制下的各数位
+        S.push(digit[n % base]); // 余数（当前的最低位）入栈
+        n /= base; // n更新为其对base的除商
+    }
+    string result;
+    while (!S.empty()) {
+        result.push_back(S.pop()); // 由高位到低位，输出栈中元素
+    }
+    return result; // 返回转换后的base进制表示
+}
 
+template<typename T> class TestStack {
+private:
+    Stack<T> myStack;
+public:
+    void testStack();  // 栈操作测试
+    void conversion(); // 进制转换
+};
+
+template<typename T>
+void TestStack<T>::testStack() {
     /* 初始化栈 */
     printf("初始化栈：\n");
     printf("[1] 请输入栈中初始元素的个数：");
@@ -101,6 +123,51 @@ int main() {
         printf("当前栈中元素个数为：%d，栈中的元素为：", myStack.size());
         print(myStack);
         printf("\n\n");
+    }
+}
+
+template<typename T>
+void TestStack<T>::conversion() {
+    cout << "进制转换：\n";
+    int caseCount = 0;
+    while (true) {
+        string prefixIn = "In [" + to_string(++caseCount) + "]: ";
+        string prefixOut = "Out[" + to_string(caseCount) + "]: ";
+        string prefixWhitespace = string(prefixIn.length(), ' ');
+
+        cout << prefixIn << "十进制正整数 n，新进制 base：";
+        size_t n;
+        int base;
+        if (cin >> n >> base) {
+            string result = convert(n, base);
+            cout << prefixOut << "转换完成，新进制表示为：";
+            cout << result + '(' + to_string(base) + ')';
+            printf("\n\n");
+        } else {
+            printf("\n\n");
+            break;
+        }
+    }
+}
+
+int main() {
+    auto obj = new TestStack<int>;
+    enum operationType {
+        Test, Conversion
+    };
+    cout << "请输入要执行的操作（01.栈测试 02.进制转换）：";
+    int opType;
+    cin >> opType;
+    cout << endl;
+    switch (--opType) {
+        case Test: {
+            obj->testStack();
+            break;
+        }
+        case Conversion: {
+            obj->conversion();
+            break;
+        }
     }
 
     return 0;
