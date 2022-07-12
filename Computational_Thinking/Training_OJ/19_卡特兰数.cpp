9ed486f99c49fa5d2c88e0b2297dac6ecc645be3
Hazuki-295 <luosheng233@outlook.com>
2022-07-13 01:34:20 +0800

    Update 19_卡特兰数.cpp

--- Computational_Thinking/Training_OJ/19_卡特兰数.cpp
+++ Computational_Thinking/Training_OJ/19_卡特兰数.cpp
@@ -3,32 +3,26 @@
 #include <vector>
 #include <algorithm>
 
-
 using namespace std;
 
 class Solution {
 private:
     vector<string> result;
 public:
-    void backtrack(string &A, string &B, string &C) {
+    void backtrack(string A, string B, string C) {
         if (A.size() == 0 && B.size() == 0) { // 递归基，所有字符都已放入C
             result.push_back(C); // 得到一个等价串
             return;
         }
 
+        /* B中栈顶元素出栈 */
         if (B.size() != 0) {
-            char temp = B.back(); // 备份栈顶元素
-            B.pop_back(); C.push_back(temp); backtrack(A, B, C); // 尝试
-            B.push_back(temp); C.pop_back(); // 回溯
+            backtrack(A, B.substr(0, B.size() - 1), C + B.back());
         }
 
+        /* A中栈顶元素出栈 */
         if (A.size() != 0) {
-            /* A中栈顶元素出栈 */
-            char temp = A.front(); A.erase(A.begin());
-
-            /* 尝试并回溯 */
-            B.push_back(temp); backtrack(A, B, C);
-            B.pop_back(); A.insert(A.begin(), temp);
+            backtrack(A.substr(1, A.size()), B + A.front(), C);
         }
     }
 
