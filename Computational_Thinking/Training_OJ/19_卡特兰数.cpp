9380062a6aaa2366b6173abd898d2a4814ed3610
Hazuki-295 <luosheng233@outlook.com>
2022-07-13 00:54:21 +0800

    Update 19_卡特兰数.cpp

--- Computational_Thinking/Training_OJ/19_卡特兰数.cpp
+++ Computational_Thinking/Training_OJ/19_卡特兰数.cpp
@@ -1,60 +1,51 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <algorithm>
 #include <unordered_set>
+
 using namespace std;
 
-class Solution
-{
+class Solution {
 private:
-	unordered_set<string> res; int length;
+    unordered_set<string> result;
 public:
-	// 从A串的第一个字符开始，依次取出串中的每个字符
-	// 可以选择将取出的字符直接放入字符串B或C的尾部
-	// 若字符串A或B中还有字符，
-	// 可以继续从A中按顺序取出单个字符放入串B或C的尾部，
-	// 也可以选择从串B的尾部取出单个字符并放入串C的尾部
-	Solution(int length) :length(length) {}
+    void backtrack(string &A, string &B, string &C) {
+        if (A.size() == 0 && B.size() == 0) { // 递归基，所有字符都已放入C
+            result.insert(C); // 得到一个等价串
+            return;
+        }
 
-	void backtrack(string &A, string &B, string &C) {
-		if (A.size() == 0 && B.size() == 0) { // 所有字符都放入C了
-			res.insert(C); // 得到一个等价串
-			return;
-		}
+        if (B.size() != 0) {
+            char temp = B.back(); // 备份栈顶元素
+            B.pop_back(); C.push_back(temp); backtrack(A, B, C); // 尝试
+            B.push_back(temp); C.pop_back(); // 回溯
+        }
 
-		if (B.size() != 0) {
-			char temp = B.back();
-			B.pop_back(); C.push_back(temp); // 尝试
-			backtrack(A, B, C);
-			B.push_back(temp); C.pop_back(); // 回溯
-		}
+        if (A.size() != 0) {
+            /* A中栈顶元素出栈 */
+            char temp = A.front(); A.erase(A.begin());
 
-		if (A.size() != 0) {
-			char temp = A.front(); A.erase(A.begin());
-			B.push_back(temp); backtrack(A, B, C); B.pop_back(); // 尝试并回溯
-			C.push_back(temp); backtrack(A, B, C); C.pop_back(); // 尝试并回溯
-			A.insert(A.begin(), temp);
-		}
-	}
+            /* 尝试并回溯 */
+            B.push_back(temp); backtrack(A, B, C);
+            B.pop_back(); A.insert(A.begin(), temp);
+        }
+    }
 
-	bool exsit(string &s) {
-		return find(res.begin(), res.end(), s) != res.end();
-	}
+    bool exist(string &s) {
+        return find(result.begin(), result.end(), s) != result.end();
+    }
 };
 
-int main()
-{
-	int cases = 0; string str, s;
-	while (cin >> str >> s) {
-		cout << "Case " << ++cases << ": ";
-		Solution *obj = new Solution(str.length());
-		string B, C; obj->backtrack(str, B, C);
+int main() {
+    int cases = 0;
+    string str, s;
+    while (cin >> str >> s) {
+        Solution *obj = new Solution();
+        string B, C;
+        obj->backtrack(str, B, C);
 
-		if (obj->exsit(s))
-			cout << "Yes" << endl;
-		else
-			cout << "No" << endl;
-	}
-	return 0;
+        cout << "Case " << ++cases << ": ";
+        cout << (obj->exist(s) ? "Yes" : "No") << endl;
+    }
+    return 0;
 }
