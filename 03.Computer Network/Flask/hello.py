f81d57a92279c9accf945f237cfecc0c341df242
Hazuki-295 <luosheng233@outlook.com>
2022-07-26 21:59:45 +0800

    Update hello.py
    Flask backend lesson 1, from luotuo-flask-bilibili.

--- 03.Computer Network/Flask/hello.py
+++ 03.Computer Network/Flask/hello.py
@@ -1,8 +1,34 @@
 from flask import Flask
+from flask import request
 
 miniApp = Flask(__name__)
 
 
 @miniApp.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "Hello, World!"
+
+
+@miniApp.route("/name", methods=['GET', 'POST'])
+def get_name():
+    if request.method == 'GET':
+        return "Hazuki-295 from GET"
+    elif request.method == 'POST':
+        return "Hazuki-295 from POST"
+
+
+@miniApp.route("/profile", methods=['GET', 'POST'])
+def get_profile():
+    if request.method == 'GET':
+        name = request.args.get("name", "None")
+        if name == "Hazuki-295":
+            return dict(name="Hazuki-295 from GET", fans=1000)
+        else:
+            return dict(name="luotuo from GET", fans=100000)
+    elif request.method == 'POST':
+        # assert request body is in json format
+        name = request.json.get("name")
+        if name == "Hazuki-295":
+            return dict(name="Hazuki-295 from POST", fans=1000)
+        else:
+            return dict(name="luotuo from POST", fans=100000)
