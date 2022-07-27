0b54fc2afd4879e6180aee33c93f1e8c7bd0ccaa
Hazuki-295 <luosheng233@outlook.com>
2022-07-28 00:40:41 +0800

    Update LinkList.h

--- 01.Data Structure/01.List/LinkList.h
+++ 01.Data Structure/01.List/LinkList.h
@@ -4,8 +4,13 @@ typedef int ElemType;
 
 /* 线性表的链式表示（单链表） */
 typedef struct ListNode {
+// 成员
     ElemType data;     // 数据域
     ListNode *next;    // 指针域
+// 构造函数
+    ListNode() {} // （默认构造函数）针对头结点的构造
+    ListNode(ElemType e, ListNode *p = nullptr)
+            : data(e), next(p) {} // 一般节点的默认构造器
 } ListNode, *LinkList; // 链表结点、链表（头指针）的类型定义
 
 /* 初始化表。构造一个空的线性表。 */
@@ -28,3 +33,38 @@ void DestroyList(LinkList &L) {
 bool Empty(LinkList L) {
     return L->next == nullptr;
 }
+
+/* 按位查找：找到表L中第i个位置的结点，并返回指向该结点的指针。 */
+ListNode *GetElem(LinkList L, int i) {
+    if (i < 0) return nullptr;
+    ListNode *p = L; // 从头结点出发
+    int index = 0;   // 当前p指向的结点的位序（头结点的位序视为0）
+    while (p != nullptr && index < i) { // 顺着指针域找到第i个结点
+        p = p->next;
+        index++;
+    }
+    return p; // 返回指向该结点的指针；若i越界将返回nullptr，若i=0将返回头结点
+} // O(n)
+
+/* 指定结点的后插操作：将e作为p的直接后继插入。 */
+bool InsertAfter(ListNode *p, ElemType e) {
+    if (p == nullptr) return false;
+    ListNode *x = new ListNode(e, p->next); // 创建新结点，以当前结点p的后继为新结点的后继
+    p->next = x; // 使new成为当前节点的后继
+    return true;
+} // O(1)
+
+/* 指定结点的前插操作：将e作为p的直接前驱插入。 */
+bool InsertBefore(ListNode *p, ElemType e) {
+    if (p == nullptr) return false;
+    ListNode *x = new ListNode(p->data, p->next); // 创建新结点，链接到p后，并拷贝p的值
+    p->next = x; // 使new成为当前节点的后继
+    p->data = e; // 更新当前结点的数据域，等效的前插操作
+    return true;
+} // O(1)
+
+/* 插入操作。在表L中的第i个位置上插入指定元素e。 */
+bool ListInsert(LinkList &L, int i, ElemType e) {
+    ListNode *p = GetElem(L, i - 1);
+    return InsertAfter(p, e);
+} // O(n)
