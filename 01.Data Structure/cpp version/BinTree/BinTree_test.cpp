dd5d03701aff58737e9399a510f644b27ca4e983
Hazuki-295 <luosheng233@outlook.com>
2022-08-02 20:06:24 +0800

    Update BinTree_test.cpp

--- 01.Data Structure/cpp version/BinTree/BinTree_test.cpp
+++ 01.Data Structure/cpp version/BinTree/BinTree_test.cpp
@@ -2,13 +2,14 @@
 #include <vector>
 #include <sstream>
 #include "BinTree.h"
-#include "../Stack_Queue/Queue.h"
+#include "../Sequence/Vector.h"
 
 using namespace std;
 
 Queue<string> readTreeNodes(string sequence) {
     Queue<string> treeNodes;
-    sequence = sequence.substr(sequence.find('[') + 1, sequence.rfind(']') - 1);
+    int begin = sequence.find('['), end = sequence.rfind(']');
+    sequence = sequence.substr(begin + 1, end - begin - 1);
     stringstream buffer(sequence);
     string token;
     while (getline(buffer, token, ',')) {
@@ -17,25 +18,42 @@ Queue<string> readTreeNodes(string sequence) {
     return treeNodes;
 }
 
-BinTree<string> createTree(Queue<string> &treeNodes) {
-    BinTree<string> binTree; // 创建空树
+BinTree<string> *createTree(Queue<string> &treeNodes) {
+    BinTree<string> *binTree = new BinTree<string>; // 创建空树
     Queue<BinNodePosi<string>> Q; // 辅助队列
 
-    BinNodePosi<string> root = binTree.insertAsRoot(treeNodes.dequeue()); // 首元素作为树根
+    BinNodePosi<string> root = binTree->insertAsRoot(treeNodes.dequeue()); // 首元素作为树根
     Q.enqueue(root); // 等待插入孩子节点
 
     while (!Q.empty()) {
         BinNodePosi<string> x = Q.dequeue();
         if (!treeNodes.empty()) {
             string lc = treeNodes.dequeue();
-            if (lc != "null") Q.enqueue(binTree.insertAsLC(x, lc));
+            if (lc != "null") Q.enqueue(binTree->insertAsLC(x, lc));
         }
         if (!treeNodes.empty()) {
             string rc = treeNodes.dequeue();
-            if (rc != "null") Q.enqueue(binTree.insertAsRC(x, rc));
+            if (rc != "null") Q.enqueue(binTree->insertAsRC(x, rc));
         }
     }
     return binTree;
+}
+
+template<typename T>
+struct PrintVector {
+    Vector<T> elems; // 元素
+    virtual void operator()(T &e) { elems.insert(e); } // 存放元素
+};
+
+template<typename T>
+ostream &operator<<(ostream &os, Vector<T> const &V) {
+    if (V.empty()) return os << "[ ]";
+    os << "[ ";
+    for (int i = 0; i < V.size() - 1; i++) {
+        os << V[i] << ", ";
+    }
+    os << V[V.size() - 1] << " ]";
+    return os;
 }
 
 template<typename T> void testBinTree() {
@@ -47,13 +65,24 @@ template<typename T> void testBinTree() {
     string sequence;
     getline(cin, sequence);
     Queue<string> treeNodes = readTreeNodes(sequence);
-    myBinTree = createTree(treeNodes);
+    myBinTree = *createTree(treeNodes);
     printf("[2] 二叉树创建完成。");
     cout << '\n' << endl;
 
     printf("遍历二叉树：\n");
+    PrintVector<T> pre, in, post, level;
+    myBinTree.travPre(pre);
     printf("[1] 先序遍历序列：");
-    // myBinTree->travPre();
+    cout << pre.elems << endl;
+    myBinTree.travIn(in);
+    printf("[2] 中序遍历序列：");
+    cout << in.elems << endl;
+    myBinTree.travPost(post);
+    printf("[3] 后序遍历序列：");
+    cout << post.elems << endl;
+    myBinTree.travLevel(level);
+    printf("[4] 层次遍历序列：");
+    cout << level.elems << endl;
 
     return;
 }
