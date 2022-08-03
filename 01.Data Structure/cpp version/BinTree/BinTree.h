b25823a894d52e41dae83f55ec0d6142dd1f9308
Hazuki-295 <luosheng233@outlook.com>
2022-08-03 17:17:08 +0800

    Update BinTree_test.cpp

--- 01.Data Structure/cpp version/BinTree/BinTree.h
+++ 01.Data Structure/cpp version/BinTree/BinTree.h
@@ -35,6 +35,7 @@ public:
     template<typename VST> void travLevel(VST &visit) { if (_root) _root->travLevel(visit); } // 层次遍历
     // 迭代式遍历算法
     template<typename VST> void travPre_I(VST &visit) { if (_root) _root->travPre_I(visit); } // 先序遍历
+    template<typename VST> void travIn_I(VST &visit) { if (_root) _root->travIn_I(visit); } // 先序遍历
 }; // BinTree
 
 /* 二叉树节点的高度更新：一旦有节点加入或离开二叉树，则更新其所有祖先的高度。 */
--- 01.Data Structure/cpp version/BinTree/BinTree_test.cpp
+++ 01.Data Structure/cpp version/BinTree/BinTree_test.cpp
@@ -77,9 +77,11 @@ template<typename T> void testBinTree() {
     cout << "[4] 层次遍历序列：" << level.elems << '\n' << endl;
 
     printf("迭代式遍历算法：\n");
-    StoreInVector<T> pre_I;
+    StoreInVector<T> pre_I, in_I;
     myBinTree.travPre_I(pre_I);
-    cout << "[1] 先序遍历序列：" << pre_I.elems << '\n' << endl;
+    cout << "[1] 先序遍历序列：" << pre_I.elems << '\n';
+    myBinTree.travIn_I(in_I);
+    cout << "[2] 中序遍历序列：" << in_I.elems << '\n';
 }
 
 int main() {
