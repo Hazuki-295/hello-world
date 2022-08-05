64ebca925f4a1168b0e8343e53f059d440fe652a
Hazuki-295 <luosheng233@outlook.com>
2022-08-05 12:38:45 +0800

    Create struct_test.c

---
+++ 04.Computer Organization/assignments/src/chapter3/struct_test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct rec {
+    int i;
+    int j;
+    int a[2];
+    int *p;
+};
+
+void print_rec(struct rec *r) {
+    printf("r -> [ i = %d, j = %d, a[0] = %d, a[1] = %d, p = %p ]\n", r->i, r->j, r->a[0], r->a[1], r->p);
+}
+
+int main() {
+    struct rec obj = {0, 1, {2, 3}, &obj.i};
+    struct rec *r = &obj;
+    int *p;
+    print_rec(r);
+
+    //r->j = r->i;    // example1
+    p = &(r->a[1]); // example2
+    r->p = &(r->a[r->i + r->j]); // example3
+
+    print_rec(r);
+    printf("%p", p);
+
+    return 0;
+}
+
