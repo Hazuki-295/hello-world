3b68aac50c3fdf6370817665a56518a807a57f13
Hazuki-295 <luosheng233@outlook.com>
2022-08-29 19:40:49 +0800

    Create code_snippets.c

---
+++ 04.Computer Organization/assignments/src/chapter3/code_snippets.c
@@ -0,0 +1,28 @@
+/* csapp 3rd - P223 Data Movement Example */
+long exchange(long *xp, long y) {
+    long x = *xp;
+    *xp = y;
+    return x;
+}
+
+/* csapp 3rd - P229 Load Effective Address, leaq instruction */
+long scale(long x, long y, long z) {
+    long t = x + 4 * y + 12 * z;
+    return t;
+}
+
+/* csapp 3rd - P231 Shift Operations */
+long shift_left4_rightn(long x, long n) {
+    x <<= 4;
+    x >>= n;
+    return x;
+}
+
+/* csapp 3rd - P232 Example of Arithmetic Operations */
+long arith(long x, long y, long z) {
+    long t1 = x ^ y;
+    long t2 = z * 48;
+    long t3 = t1 & 0x0F0F0F0F;
+    long t4 = t2 - t3;
+    return t4;
+}
