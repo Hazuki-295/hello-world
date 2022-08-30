155448d2a8ea78ce2d473d0ddc6fe3776486c26b
Hazuki-295 <luosheng233@outlook.com>
2022-08-30 16:22:30 +0800

    Update code_snippets.c

--- 04.Computer Organization/assignments/src/chapter3/code_snippets.c
+++ 04.Computer Organization/assignments/src/chapter3/code_snippets.c
@@ -43,3 +43,10 @@ void remdiv(long x, long y, long *qp, long *rp) {
     *qp = q;
     *rp = r;
 }
+
+void uremdiv(unsigned long x, unsigned long y, unsigned long *qp, unsigned long *rp) {
+    unsigned long q = x / y;
+    unsigned long r = x % y;
+    *qp = q;
+    *rp = r;
+}
