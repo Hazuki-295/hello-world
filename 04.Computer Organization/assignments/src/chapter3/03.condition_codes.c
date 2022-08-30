2de3ca8ad7d1c60b6a307c325ea11338eb92fc2c
Hazuki-295 <luosheng233@outlook.com>
2022-08-30 21:00:21 +0800

    Update 03.condition_codes.c

--- 04.Computer Organization/assignments/src/chapter3/03.condition_codes.c
+++ 04.Computer Organization/assignments/src/chapter3/03.condition_codes.c
@@ -1,56 +1,42 @@
-int x, y;
-
 typedef long data_t;
 
 #define COMP <
 
-/* $begin 110-cc-c */
+/* csapp 3rd edition - P240 Practice Problem 3.13 */
 int comp(data_t a, data_t b) {
     return a COMP b;
 }
 
-/* $end 110-cc-c */
-
-int ilt(int a, int b) {
+int int_less_than(int a, int b) { // A
     return a < b;
 }
 
-
-int bge(short a, short b) {
+int short_greater_or_equal(short a, short b) { // B
     return a >= b;
 }
 
-
-int ucle(unsigned char a, unsigned char b) {
+int unsigned_char_less_or_equal(unsigned char a, unsigned char b) { // C
     return a <= b;
 }
 
-
-int lieq(long a, long b) {
+int long_int_equal(long a, long b) { // D
     return a == b;
 }
 
-int ulieq(unsigned long a, unsigned long b) {
+int unsigned_long_int_equal(unsigned long a, unsigned long b) { // D
     return a == b;
 }
 
-int peq(char *a, char *b) {
+int pointer_equal(char *a, char *b) { // D
     return a == b;
-}
-
-
-int gt(unsigned long a, unsigned long b) {
-    return a > b;
 }
 
 #define TEST !=
 
-/* $begin 110-cc2-c */
+/* csapp 3rd edition - P240 Practice Problem 3.14 */
 int test(data_t a) {
     return a TEST 0;
 }
-
-/* $end 110-cc2-c */
 
 int test_lige(long a) {
     return a >= 0;
