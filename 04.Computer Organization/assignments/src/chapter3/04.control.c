7ae6fe69cfe2795b0746239d3570dbe0a5ecb587
Hazuki-295 <luosheng233@outlook.com>
2022-08-31 19:43:39 +0800

    Update 04.control.c

--- 04.Computer Organization/assignments/src/chapter3/04.control.c
+++ 04.Computer Organization/assignments/src/chapter3/04.control.c
@@ -1,3 +1,35 @@
+/* csapp 3rd edition - P246 Conditional Control */
+long lt_cnt = 0;
+long ge_cnt = 0;
+
+long absdiff_se(long x, long y) {
+    long result;
+    if (x < y) {
+        lt_cnt++;
+        result = y - x;
+    } else {
+        ge_cnt++;
+        result = x - y;
+    }
+    return result;
+}
+
+long gotodiff_se(long x, long y) { // goto version of absdiff_se()
+    long result;
+    if (x >= y) goto x_ge_y;
+    {
+        lt_cnt++;
+        result = y - x;
+        return result;
+    }
+    x_ge_y:
+    {
+        ge_cnt++;
+        result = x - y;
+        return result;
+    }
+}
+
 /* csapp 3rd edition - P248 Practice Problem 3.16 */
 void cond(long a, long *p) {
     if (p && a < *p) {
@@ -5,13 +37,11 @@ void cond(long a, long *p) {
     }
 }
 
-void goto_cond(long a, long *p) { // goto version of cond
-    if (p == 0) {
+void goto_cond(long a, long *p) { // goto version of cond()
+    if (p == 0)
         goto done;
-    }
-    if (a >= *p) {
+    if (a >= *p)
         goto done;
-    }
     *p = a;
     done:
     return;
