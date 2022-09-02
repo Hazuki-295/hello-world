67f040679a0a6725923115cd8f6c9421e629cdcb
Hazuki-295 <luosheng233@outlook.com>
2022-09-02 22:47:24 +0800

    Update 05.loop.c

--- 04.Computer Organization/assignments/src/chapter3/05.loop.c
+++ 04.Computer Organization/assignments/src/chapter3/05.loop.c
@@ -51,16 +51,16 @@ done:
 
 /* csapp 3rd edition - P265 for loop version of factorial program */
 long fact_for(long n) {
-    long i;
     long result = 1;
-    for (i = 2; i <= n; i++)
+    for (long i = 2; i <= n; i++) {
         result *= i;
+    }
     return result;
 }
 
-long fact_for_while(long n) {
-    long i = 2;
+long fact_for_while(long n) { // equivalent while loop
     long result = 1;
+    long i = 2;
     while (i <= n) {
         result *= i;
         i++;
@@ -68,38 +68,26 @@ long fact_for_while(long n) {
     return result;
 }
 
-long fact_for_jm_goto(long n) {
-    long i = 2;
+long fact_for_jm_goto(long n) { // jump-to-middle translation
     long result = 1;
+    long i = 2;
     goto test;
 loop:
     result *= i;
     i++;
 test:
-    if (i <= n)
-        goto loop;
+    if (i <= n) goto loop;
     return result;
 }
 
-long fact_for_gd_goto(long n) {
-    long i = 2;
+long fact_for_gd_goto(long n) { // guarded-do translation
     long result = 1;
-    if (n <= 1)
-        goto done;
+    long i = 2;
+    if (n <= 1) goto done;
 loop:
     result *= i;
     i++;
-    if (i <= n)
-        goto loop;
+    if (i <= n) goto loop;
 done:
-    return result;
-}
-
-long rfact(long n) {
-    long result;
-    if (n <= 1)
-        result = 1;
-    else
-        result = n * rfact(n - 1);
     return result;
 }
