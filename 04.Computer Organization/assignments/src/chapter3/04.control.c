2a482ac47589f2cc61dbb6de7120f0c894803d60
Hazuki-295 <luosheng233@outlook.com>
2022-09-02 16:23:54 +0800

    Update 04.control.c

--- 04.Computer Organization/assignments/src/chapter3/04.control.c
+++ 04.Computer Organization/assignments/src/chapter3/04.control.c
@@ -1,4 +1,4 @@
-/* csapp 3rd edition - P246 Conditional Control */
+/* csapp 3rd edition - P246 Implementing Conditional Branches With Conditional Control */
 long lt_cnt = 0;
 long ge_cnt = 0;
 
@@ -14,7 +14,7 @@ long absdiff_se(long x, long y) {
     return result;
 }
 
-long gotodiff_se(long x, long y) { // goto version of absdiff_se()
+long gotodiff_se(long x, long y) {
     long result;
     if (x >= y) goto x_ge_y;
     {
@@ -37,7 +37,7 @@ void cond(long a, long *p) {
     }
 }
 
-void goto_cond(long a, long *p) { // goto version of cond()
+void goto_cond(long a, long *p) {
     if (p == 0)
         goto done;
     if (a >= *p)
@@ -45,4 +45,22 @@ void goto_cond(long a, long *p) { // goto version of cond()
     *p = a;
     done:
     return;
+}
+
+/* csapp 3rd edition - P250 Implementing Conditional Branches With Conditional Moves */
+long absdiff(long x, long y) { // using -O1 flag instead of -Og
+    long result;
+    if (x < y)
+        result = y - x;
+    else
+        result = x - y;
+    return result;
+}
+
+long cmovdiff(long x, long y) {
+    long rval = y - x;
+    long eval = x - y;
+    long ntest = x >= y;
+    if (ntest) rval = eval; // This statement requires single instruction
+    return rval;
 }
