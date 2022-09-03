222441917de90596051cb396905b76cb3ce2ea26
Hazuki-295 <luosheng233@outlook.com>
2022-09-03 21:03:23 +0800

    Update 06.procedure.c

--- 04.Computer Organization/assignments/src/chapter3/06.procedure.c
+++ 04.Computer Organization/assignments/src/chapter3/06.procedure.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* csapp 3rd edition - P279 program involving procedure calls and returns */
+/* csapp 3rd edition - P279 Program involving procedure calls and returns */
 long leaf(long y) {
     return y + 2;
 }
@@ -13,4 +13,22 @@ int main(int argc, char *argv[]) {
     long cval = top(100);
     printf("top(100) --> %ld\n", cval);
     return 0;
+}
+
+/* csapp 3rd edition - P283 Example of function with multiple arguments of different types */
+void proc(long a1, long *a1p,
+          int  a2, int  *a2p,
+          short a3, short *a3p,
+          char  a4, char  *a4p) {
+    *a1p += a1;
+    *a2p += a2;
+    *a3p += a3;
+    *a4p += a4;
+}
+
+long call_proc() {
+    long  x1 = 1; int  x2 = 2;
+    short x3 = 3; char x4 = 4;
+    proc(x1, &x1, x2, &x2, x3, &x3, x4, &x4);
+    return (x1 + x2) * (x3 - x4);
 }
