1d84f459f6f24578f31b72fa08f5d22505bf6cd8
Hazuki-295 <luosheng233@outlook.com>
2022-09-05 18:42:25 +0800

    Update 07.array.c

--- 04.Computer Organization/assignments/src/chapter3/07.array.c
+++ 04.Computer Organization/assignments/src/chapter3/07.array.c
@@ -26,3 +26,28 @@ int fix_prod_ele_opt(fix_matrix A, fix_matrix B, long i, long k) {
     } while (Bptr != Bend);       /* Test for stopping point  */
     return result;
 }
+
+/* csapp 3rd edition - P300 Code computing matrix product for variable-size arrays */
+/* Compute i,k of variable matrix product, original C code */
+int var_prod_ele(long n, int A[n][n], int B[n][n], long i, long k) {
+    long j;
+    int result = 0;
+
+    for (j = 0; j < n; j++)
+        result += A[i][j] * B[j][k];
+
+    return result;
+}
+
+/* Compute i,k of variable matrix product, optimized C code */
+int var_prod_ele_opt(long n, int A[n][n], int B[n][n], long i, long k) {
+    int *Arow = A[i];
+    int *Bptr = &B[0][k];
+    int result = 0;
+    long j;
+    for (j = 0; j < n; j++) {
+        result += Arow[j] * *Bptr;
+        Bptr += n;
+    }
+    return result;
+}
