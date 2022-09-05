1bca0960ce5b0bd473702e18078129fa32e150a2
Hazuki-295 <luosheng233@outlook.com>
2022-09-05 15:21:57 +0800

    Create 07.array.c

---
+++ 04.Computer Organization/assignments/src/chapter3/07.array.c
@@ -0,0 +1,28 @@
+/* csapp 3rd edition - P297 Code computing matrix product for fixed-length arrays */
+#define N 16
+typedef int fix_matrix[N][N];
+
+/* Compute i,k of fixed matrix product, original C code */
+int fix_prod_ele(fix_matrix A, fix_matrix B, long i, long k) {
+    long j;
+    int result = 0;
+
+    for (j = 0; j < N; j++)
+        result += A[i][j] * B[j][k];
+
+    return result;
+}
+
+/* Compute i,k of fixed matrix product, optimized C code */
+int fix_prod_ele_opt(fix_matrix A, fix_matrix B, long i, long k) {
+    int *Aptr = &A[i][0];    /* Points to elements in row i of A    */
+    int *Bptr = &B[0][k];    /* Points to elements in column k of B */
+    int *Bend = &B[N][k];    /* Marks stopping point for Bptr       */
+    int result = 0;
+    do {                          /* No need for initial test */
+        result += *Aptr * *Bptr;  /* Add next product to sum  */
+        Aptr++;                   /* Move Aptr to next column */
+        Bptr += N;                /* Move Bptr to next row    */
+    } while (Bptr != Bend);       /* Test for stopping point  */
+    return result;
+}
