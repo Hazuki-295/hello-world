27b602a7f3661c2a764012469bd694af9b9ee1be
Hazuki-295 <luosheng233@outlook.com>
2022-09-22 12:40:18 +0800

    Update main.c

--- 04.Computer Organization/assignments/src/chapter7/relocatable/main.c
+++ 04.Computer Organization/assignments/src/chapter7/relocatable/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-int count = 10;
-int value;
+int value1 = 10; /* strong symbol - .data section */
+int value2 = 0;  /* strong symbol - .bss section */
+int value3;      /* weak symbol - COMMON */
 
 void func(int sum) {
     printf("sum is:%d\n", sum);
 }
 
 int main() {
-    static int a = 1;
-    static int b = 0;
+    static int a = 1; /* .data section */
+    static int b = 0; /* .bss section */
     int x = 1;
     func(a + b + x);
     return 0;
