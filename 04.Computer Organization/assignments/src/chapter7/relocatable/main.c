8abf5834655098d1bb904dd340a73c7058493803
Hazuki-295 <luosheng233@outlook.com>
2022-09-22 14:36:14 +0800

    Update main.c

--- 04.Computer Organization/assignments/src/chapter7/relocatable/main.c
+++ 04.Computer Organization/assignments/src/chapter7/relocatable/main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int value1 = 10; /* strong symbol - .data section */
-int value2 = 0;  /* strong symbol - .bss section */
-int value3;      /* weak symbol - COMMON */
+int value1 = 1; /* strong symbol - .data section */
+int value2 = 0; /* strong symbol - .bss section */
+int value3;     /* weak symbol - COMMON */
 
 void func(int sum) {
     printf("sum is:%d\n", sum);
 }
 
 int main() {
-    static int a = 1; /* .data section */
-    static int b = 0; /* .bss section */
-    int x = 1;
+    static int a = 1; /* static local variable - .data section */
+    static int b = 0; /* static local variable - .bss section */
+    int x = 1; /* nonstatic local variable - run time stack */
     func(a + b + x);
     return 0;
 }
