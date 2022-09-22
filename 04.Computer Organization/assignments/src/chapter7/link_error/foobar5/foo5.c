0f774ff600dd375fdba4c1ac17f981ccb8244fdc
Hazuki-295 <luosheng233@outlook.com>
2022-09-22 20:20:49 +0800

    Update foo5.c

--- 04.Computer Organization/assignments/src/chapter7/link_error/foobar5/foo5.c
+++ 04.Computer Organization/assignments/src/chapter7/link_error/foobar5/foo5.c
@@ -5,8 +5,28 @@ void f();
 int y = 15212;
 int x = 15213;
 
+typedef struct {
+    int x;
+    int y;
+} internal;
+
+internal double2ints(double d) {
+    union {
+        internal ints;
+        double d;
+    } temp;
+    temp.d = d;
+    return temp.ints;
+}
+
+void test_union() {
+    internal s = double2ints(-0.0);
+    printf("x = 0x%x y = 0x%x \n", s.x, s.y);
+}
+
 int main() {
     f();
     printf("x = 0x%x y = 0x%x \n", x, y);
+    test_union();
     return 0;
 }
