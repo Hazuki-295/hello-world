2cb8fd6df552e913c5ca8334eb790fe39810a5ee
Hazuki-295 <luosheng233@outlook.com>
2022-09-23 17:30:23 +0800

    Update main.c

--- 04.Computer Organization/assignments/src/chapter7/relocatable/main.c
+++ 04.Computer Organization/assignments/src/chapter7/relocatable/main.c
@@ -11,7 +11,8 @@ void func(int sum) {
 int main() {
     static int a = 1; /* static local variable - .data section */
     static int b = 0; /* static local variable - .bss section */
-    int x = 1; /* nonstatic local variable - run time stack */
-    func(a + b + x);
+    static int c;     /* static local variable - .bss section */
+    int x = 12345; /* nonstatic local variable - run time stack */
+    func(a + b + c + x);
     return 0;
 }
