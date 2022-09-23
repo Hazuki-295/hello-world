616d4e546161d2db34dc23699ceb02fc5247612a
Hazuki-295 <luosheng233@outlook.com>
2022-09-23 21:07:34 +0800

    Update chapter7/symbols

--- 04.Computer Organization/assignments/src/chapter7/symbols/main.c
+++ 04.Computer Organization/assignments/src/chapter7/symbols/main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
+int sum(int *a, int n);
+
+int array[2] = {295, 200};
+
 int value1 = 1; /* strong symbol - .data section */
 int value2 = 0; /* strong symbol - .bss section */
 int value3;     /* weak symbol - COMMON */
-
-void func(int sum) {
-    printf("sum is:%d\n", sum);
-}
 
 int main() {
     static int a = 1; /* static local variable - .data section */
     static int b = 0; /* static local variable - .bss section */
     static int c;     /* static local variable - .bss section */
     int x = 12345; /* nonstatic local variable - run time stack */
-    func(a + b + c + x);
+
+    int var = sum(array, 2);
+    printf("sum is:%d\n", var + a + b + c + x);
     return 0;
 }
