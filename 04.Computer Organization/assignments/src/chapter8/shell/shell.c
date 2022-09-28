960af6bef5b08c905c22c01c67953b4b1ccc45b7
Hazuki-295 <luosheng233@outlook.com>
2022-09-28 23:21:59 +0800

    Update shell.c

--- 04.Computer Organization/assignments/src/chapter8/shell/shell.c
+++ 04.Computer Organization/assignments/src/chapter8/shell/shell.c
@@ -8,7 +8,8 @@
 #define MAXLINE 8192 /* Max text line length */
 #define MAXARGS 128  /* Max argument number */
 
-extern char **environ; /* Defined by libc */
+extern char **environ;     /* Defined by libc */
+volatile sig_atomic_t pid; /* global variable pid */
 
 /* Function prototypes */
 void eval(const char *cmdline); // Evaluate a command line
@@ -17,68 +18,41 @@ int builtin_command(char *argv[]);
 
 void unix_error(char *msg);
 
+void sigchld_handler(int s) { /* SIGCHLD handler */
+    int olderrno = errno;
+    pid = waitpid(-1, NULL, 0);
+    errno = olderrno;
+}
+
+void sigint_handler(int sig) { /* SIGINT handler */
+    printf("\nHave a nice day!\n");
+    exit(0);
+}
+
 int main() {
+    sigset_t mask, prev;
+
+    signal(SIGCHLD, sigchld_handler);
+    signal(SIGINT, sigint_handler);
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGCHLD);
+
     char cmdline[MAXLINE]; /* Command line */
+
     while (1) {
         /* Print a command-line prompt */
         printf("> ");
+        fflush(stdout);
 
         /* Read: read a command line from the user */
         fgets(cmdline, MAXLINE, stdin);
         if (feof(stdin)) {
+            printf("\nCompleted.\n");
             exit(0);
         }
         /* Evaluate: parse the space-separated command line and run programs */
         eval(cmdline);
     }
-}
-
-/* eval - Evaluate a command line */
-void eval(const char *cmdline) {
-    char *argv[MAXARGS];  /* Argument list execve() */
-    char buffer[MAXLINE]; /* Holds modified command line */
-    int background;       /* Should the job run in background or foreground? */
-    pid_t pid;            /* Process id */
-
-    strcpy(buffer, cmdline);
-    background = parseline(buffer, argv);
-    if (argv[0] == NULL) {  /* Ignore empty lines */
-        return;
-    }
-
-    if (!builtin_command(argv)) {
-        if ((pid = fork()) == 0) {  /* Child runs user job */
-            if (execve(argv[0], argv, environ) < 0) {
-                printf("%s: Command not found.\n", argv[0]);
-                exit(0);
-            }
-        }
-
-        /* Parent waits for foreground job to terminate */
-        if (!background) {
-            int status;
-            if (waitpid(pid, &status, 0) < 0)
-                unix_error("error: waitpid error");
-        } else {
-            printf("%d %s", pid, cmdline);
-        }
-    }
-}
-
-/* If first arg is a builtin command, run it and return true */
-int builtin_command(char **argv) {
-    if (!strcmp(argv[0], "pwd")) {  /* pwd command */
-        char *pwd = getenv("PWD");
-        if (pwd != NULL) {
-            printf("%s\n", pwd);
-        }
-        return 1;
-    }
-    if (!strcmp(argv[0], "quit")) /* quit command */
-        exit(0);
-    if (!strcmp(argv[0], "&"))    /* Ignore singleton & */
-        return 1;
-    return 0;                     /* Not a builtin command */
 }
 
 /* parseline - Parse the command line and build the argv array */
@@ -114,6 +88,48 @@ int parseline(char *buffer, char **argv) {
         argv[--argc] = NULL; /* Remove argument & */
     }
     return background;
+}
+
+void execute(const char *filename, char *argv[], int background) {
+    if ((pid = fork()) == 0) {  /* Child runs user job */
+        if (execve(filename, argv, environ) < 0) {
+            printf("%s: Command not found.\n", argv[0]);
+            exit(0);
+        }
+    }
+
+    /* Parent waits for foreground job to terminate */
+    if (!background) {
+        int status;
+        if (waitpid(pid, &status, 0) < 0)
+            unix_error("error: waitpid error");
+    } else {
+        printf("%d %s", pid, filename);
+    }
+}
+
+/* eval - Evaluate a command line */
+void eval(const char *cmdline) {
+    char *argv[MAXARGS];  /* Argument list execve() */
+    char buffer[MAXLINE]; /* Holds modified command line */
+    int background;       /* Should the job run in background or foreground? */
+    pid_t pid;            /* Process id */
+
+    strcpy(buffer, cmdline);
+    background = parseline(buffer, argv);
+    if (argv[0] == NULL || !strcmp(argv[0], "&")) {  /* Ignore empty lines and singleton & */
+        return;
+    }
+
+    if (!strcmp(argv[0], "quit")) /* quit command */
+        exit(0);
+
+    char builtin[MAXLINE] = "/bin/";
+    if (access(strcat(builtin, argv[0]), F_OK) == 0) {  /* If first arg is a builtin command */
+        execute(builtin, argv, background);
+    } else {
+        execute(argv[0], argv, background);
+    }
 }
 
 void unix_error(char *msg) {  /* Unix-style error */
