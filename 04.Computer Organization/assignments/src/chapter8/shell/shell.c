bca97df153ac85a13a3d31c5396dde22822da8dc
Hazuki-295 <luosheng233@outlook.com>
2022-09-29 17:16:50 +0800

    Update shell.c

--- 04.Computer Organization/assignments/src/chapter8/shell/shell.c
+++ 04.Computer Organization/assignments/src/chapter8/shell/shell.c
@@ -103,17 +103,17 @@ void execute(const char *filename, char *argv[], int background) {
 
     /* Parent */
     pid_t pid_backup = pid;
-    pid = 0;
-    sigprocmask(SIG_SETMASK, &prev, NULL);  /* Unblock SIGCHLD */
+    pid = 0;  /* SIGCHLD is blocked at this point, which avoid the race between parent and child */
     if (!background) {
-        pid_fore = pid_backup;
+        pid_fore = pid_backup; /* Same as pid, avoid the race */
         while (!pid) {
-            sigsuspend(&prev);  /* waits for foreground job to terminate */
+            sigsuspend(&prev);  /* Temporarily unblock the SIGCHLD */
         }
     } else {
         pid_back = pid_backup;
         printf("[1] %d %s\n", pid_back, argv[0]);
     }
+    sigprocmask(SIG_SETMASK, &prev, NULL);  /* Unblock SIGCHLD */
 }
 
 /* eval - Evaluate a command line */
