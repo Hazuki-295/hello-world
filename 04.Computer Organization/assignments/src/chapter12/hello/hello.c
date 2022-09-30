2f82c74c7f25d6b0fceeae9beb8824bf2677e6ad
Hazuki-295 <luosheng233@outlook.com>
2022-09-30 16:46:34 +0800

    Create chapter12/hello

---
+++ 04.Computer Organization/assignments/src/chapter12/hello/hello.c
@@ -0,0 +1,18 @@
+/* hello.c - Pthreads "hello, world" program */
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *thread(void *vargp);
+
+int main() {
+    pthread_t tid;
+    pthread_create(&tid, NULL, thread, NULL);
+    pthread_join(tid, NULL);
+    exit(0);
+}
+
+void *thread(void *vargp) {  /* thread routine */
+    printf("Hello, world!\n");
+    return NULL;
+}
