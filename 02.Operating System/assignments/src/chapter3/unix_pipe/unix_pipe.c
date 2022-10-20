10424ffa01e6648a2533f034cab084b3aa2fda02
Hazuki-295 <luosheng233@outlook.com>
2022-10-20 19:37:17 +0800

    Update unix_pipe.c

--- 02.Operating System/assignments/src/chapter3/unix_pipe/unix_pipe.c
+++ 02.Operating System/assignments/src/chapter3/unix_pipe/unix_pipe.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
+#define MSG_NUM 4
 #define BUFFER_SIZE 25
 #define READ_END  0
 #define WRITE_END 1
 
 /* Example program demonstrating UNIX pipes. (Figures 3.21 & 3.22) */
 int main() {
-    char write_msg[BUFFER_SIZE] = "Greetings";
+    char write_msg[MSG_NUM][BUFFER_SIZE] = {"Greetings.",
+                                            "Studying ",
+                                            "Operating Systems ",
+                                            "Is Fun!"};
     char read_msg[BUFFER_SIZE];
+
     pid_t pid;
     int fd[2];
 
@@ -32,20 +38,33 @@ int main() {
         close(fd[READ_END]);
 
         /* write to the pipe */
-        write(fd[WRITE_END], write_msg, strlen(write_msg) + 1);
+        int write_num;
+        for (int i = 0; i < MSG_NUM; i++) {
+            write_num = write(fd[WRITE_END], write_msg[i], BUFFER_SIZE);
+            printf("Parent has write %d bytes: %s\n", write_num, write_msg[i]);
+        }
 
         /* close the write end of the pipe */
         close(fd[WRITE_END]);
+
+        /* parent will wait for the child to complete */
+        wait(NULL);
+
+        printf("Parent terminated.\n");
     } else {  /* child process */
         /* close the unused end of the pipe */
         close(fd[WRITE_END]);
 
         /* read from the pipe */
-        read(fd[READ_END], read_msg, BUFFER_SIZE);
-        printf("child read %s\n", read_msg);
+        int read_num;
+        while ((read_num = read(fd[READ_END], read_msg, BUFFER_SIZE)) > 0) {
+            printf("Child read %d bytes: %s\n", read_num, read_msg);
+        }
 
         /* close the read end of the pipe */
         close(fd[READ_END]);
+
+        printf("Child terminated.\n");
     }
 
     return 0;
