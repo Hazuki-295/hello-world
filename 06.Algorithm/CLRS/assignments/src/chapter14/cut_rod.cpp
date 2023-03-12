643d0770c71c7ebfd135f16a6d5d57a8acb86516
Hazuki-295 <luosheng233@outlook.com>
2023-03-12 20:09:27 +0800

    Update cut_rod.cpp

--- 06.Algorithm/CLRS/assignments/src/chapter14/cut_rod.cpp
+++ 06.Algorithm/CLRS/assignments/src/chapter14/cut_rod.cpp
@@ -15,6 +15,31 @@ int cut_rod(vector<int> &prices, int n) {
     return q;
 }
 
+/* Recursive top-down with memoization implementation of cut_rod. */
+class Memoized_Cut_Rod {
+public:
+    static int memoized_cut_rod(vector<int> &prices, int n) {
+        vector<int> remember(n + 1, -1); // will remember solution values in r
+        return memoized_cut_rod_aux(prices, n, remember);
+    }
+
+    static int memoized_cut_rod_aux(vector<int> &prices, int n, vector<int> &remember) {
+        if (remember[n] >= 0) { // already have a solution for length n?
+            return remember[n];
+        }
+
+        /* Compute q like in cut_rod. */
+        if (n == 0) return 0; // Base case.
+
+        int q = -1;
+        for (int i = 1; i <= n; i++) { // i is the position of the first cut
+            q = max(q, prices[i] + memoized_cut_rod_aux(prices, n - i, remember));
+        }
+        remember[n] = q; // remember the solution value for length n
+        return q;
+    }
+};
+
 
 int main() {
     /* Textbook example. */
@@ -24,13 +49,17 @@ int main() {
     cout << "Standard cut rod:\n";
     cout << cut_rod(prices, 4) << '\n';   // should be 10
     cout << cut_rod(prices, 10) << '\n';  // should be 30
+    // Memoized cut rod.
+    cout << "Memoized cut rod:\n";
+    cout << Memoized_Cut_Rod::memoized_cut_rod(prices, 4) << '\n';   // should be 10
+    cout << Memoized_Cut_Rod::memoized_cut_rod(prices, 10) << '\n';  // should be 30
 
     cout << endl;
 
     /* Larger example. */
     srand(time(NULL));
 
-    int size = 40;
+    int size = 30;
     prices = vector<int>(size);
     for (int i = 0; i < prices.size(); i++) {
         prices[i] = rand() % 100 + 1;
@@ -39,11 +68,20 @@ int main() {
     sort(prices.begin(), prices.end()); // sorted, unique prices
     prices.erase(unique(prices.begin(), prices.end()), prices.end());
 
+    cout << "Vector Size: " << prices.size() << '\n' << "[ ";
+    for (int i = 0; i < prices.size(); i++) {
+        cout << prices[i] << ' ';
+    }
+    cout << ']' << '\n' << endl;
+
     // Standard cut rod.
     cout << "Standard cut rod:\n";
     cout << cut_rod(prices, 18) << '\n';
-    cout << cut_rod(prices, 10) << '\n';
     cout << cut_rod(prices, prices.size()) << '\n';
+    // Memoized cut rod.
+    cout << "Memoized cut rod:\n";
+    cout << Memoized_Cut_Rod::memoized_cut_rod(prices, 18) << '\n';
+    cout << Memoized_Cut_Rod::memoized_cut_rod(prices, prices.size()) << '\n';
 
     return 0;
 }
