9f6ae36764b5e393d7f0446437afc05ac91d10b2
Hazuki-295 <luosheng233@outlook.com>
2023-03-24 22:43:44 +0800

    Update 06.Algorithm/LeetCode/前缀和 & 差分

--- 06.Algorithm/LeetCode/前缀和 & 差分/303. Range Sum Query - Immutable.cpp
+++ 06.Algorithm/LeetCode/前缀和 & 差分/303. Range Sum Query - Immutable.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 class NumArray {
 public:
-    vector<int> prefixSums; // prefixSums[i + 1] = nums[0] + nums[1] + ... + nums[i], prefixSums[0] = 0
+    vector<int> prefixSum; // prefixSum[i + 1] = nums[0] + nums[1] + ... + nums[i], prefixSum[0] = 0
 
     NumArray(vector<int> &nums) {
         int n = nums.size();
-        prefixSums.resize(n + 1);
-        /* preprocessing, calculating prefix sums */
+        prefixSum.resize(n + 1);
+        /* preprocessing, calculating prefix sum */
         for (int i = 0; i < n; i++) {
-            prefixSums[i + 1] = prefixSums[i] + nums[i];
-            // prefixSums[i] = prefixSums[i - 1] + nums[i] (for array index 1:n)
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+            // prefixSum[i] = prefixSum[i - 1] + nums[i] (for array index 1:n)
         }
     }
 
     int sumRange(int left, int right) {
-        return prefixSums[right + 1] - prefixSums[left];
-        // prefixSums[right] - prefixSums[left - 1] (for array index 1:n)
+        return prefixSum[right + 1] - prefixSum[left];
+        // prefixSum[right] - prefixSum[left - 1] (for array index 1:n)
     }
 };
--- 06.Algorithm/LeetCode/前缀和 & 差分/304. Range Sum Query 2D - Immutable.cpp
+++ 06.Algorithm/LeetCode/前缀和 & 差分/304. Range Sum Query 2D - Immutable.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 class NumMatrix {
 public:
-    vector<vector<int>> prefixSums; // prefixSums[i + 1][j + 1] = matrix[0][0] + ... + matrix[i][j], prefixSums[0][0] = 0
+    vector<vector<int>> prefixSum; // prefixSum[i + 1][j + 1] = matrix[0][0] + ... + matrix[i][j], prefixSum[0][0] = 0
 
     NumMatrix(vector<vector<int>> &matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        prefixSums.resize(m + 1, vector<int>(n + 1));
-        /* preprocessing, calculating prefix sums */
+        prefixSum.resize(m + 1, vector<int>(n + 1));
+        /* preprocessing, calculating prefix sum */
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                prefixSums[i + 1][j + 1] = prefixSums[i][j + 1] + prefixSums[i + 1][j] - prefixSums[i][j] + matrix[i][j];
-                // prefixSums[i][j] = prefixSums[i - 1][j] + prefixSums[i][j - 1] - prefixSums[i - 1][j - 1] + matrix[i][j] (for array index 1:n)
+                prefixSum[i + 1][j + 1] = prefixSum[i][j + 1] + prefixSum[i + 1][j] - prefixSum[i][j] + matrix[i][j];
+                // prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + matrix[i][j] (for array index 1:n)
             }
         }
     }
 
     int sumRegion(int x1, int y1, int x2, int y2) {
-        return prefixSums[x2 + 1][y2 + 1] - prefixSums[x1][y2 + 1] - prefixSums[x2 + 1][y1] + prefixSums[x1][y1];
-        // prefixSums[x2][y2] - prefixSums[x1 - 1][y2] - prefixSums[x2][y1 - 1] + prefixSums[x1 - 1][y1 - 1] (for array index 1:n)
+        return prefixSum[x2 + 1][y2 + 1] - prefixSum[x1][y2 + 1] - prefixSum[x2 + 1][y1] + prefixSum[x1][y1];
+        // prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1] (for array index 1:n)
     }
 };
